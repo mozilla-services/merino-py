f58af18199c66a4ab9dca39449443798e1eb784d
Wil Stuckey <wstuckey@mozilla.com>
2022-08-25 14:49:16 -0500

    fix exception

--- merino/middleware/metrics.py
+++ merino/middleware/metrics.py
@@ -1,15 +1,9 @@
 import logging
-from typing import Optional
 
 from fastapi import HTTPException
-from starlette.middleware.base import (
-    BaseHTTPMiddleware,
-    DispatchFunction,
-    RequestResponseEndpoint,
-)
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
-from starlette.types import ASGIApp
 
 from merino.metrics import get_client
 
@@ -18,27 +12,18 @@ logger = logging.getLogger(__name__)
 
 class MetricsMiddleware(BaseHTTPMiddleware):
 
-    _metric_names: dict[str, str]
-
-    def __init__(
-        self, app: ASGIApp, dispatch: Optional[DispatchFunction] = None
-    ) -> None:
-        super().__init__(app, dispatch)
-        self._metric_names = {
-            route.path: route.path.lstrip("/").replace("/", ".") for route in app.routes
-        }
+    _metric_names: dict[str, str] = {}
 
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> Response:
         client = get_client()
-        metric_name = self._metric_names[request.url.path]
+        metric_name = self.build_metric_name(request.url.path)
         status_code = 0
 
         try:
             with client.timeit(f"{metric_name}.timing"):
                 response = await call_next(request)
-            client.increment(f"{metric_name}.status_codes.{response.status_code}")
             status_code = response.status_code
         except HTTPException as e:
             status_code = e.status_code
@@ -46,3 +31,8 @@ class MetricsMiddleware(BaseHTTPMiddleware):
         finally:
             client.increment(f"{metric_name}.status_codes.{status_code}")
         return response
+
+    def build_metric_name(self, path: str) -> str:
+        if path not in self._metric_names:
+            self._metric_names[path] = path.lower().lstrip("/").replace("/", ".")
+        return self._metric_names[path]
