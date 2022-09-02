b13dbfb84d65ca454bfb5e5b8dfd303ce8575de5
Tif Tran <ttran@mozilla.com>
2022-09-02 12:24:27 -0700

    Update merino/remotesettings.py
    Co-authored-by: Nan Jiang <najiang@mozilla.com>

--- merino/remotesettings.py
+++ merino/remotesettings.py
@@ -32,7 +32,11 @@ class Client:
         return await self.client.get_records(collection=collection, bucket=bucket)
 
     async def fetch_attachment(self, attachement_uri) -> httpx.Response:
-        """TODO: Get attachment"""
+        """Fetch an attachment from Remote Settings server for a given URI.
+        
+        Args:
+          - `attachment_uri`: the URI of the attachment
+        """
         if not self.attachment_host:
             self.attachment_host = await self.fetch_attachment_host()
         uri = urljoin(self.attachment_host, attachement_uri)
