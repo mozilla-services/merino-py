02aa07aa7c8ad9b9712b65b8b86e77ee5d36f2d6
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2022-11-02 14:05:58 -0400

    test: apply PR recommendations for TestClient fixtures

--- tests/integration/api/conftest.py
+++ tests/integration/api/conftest.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from typing import Iterator
+
 import pytest
 from starlette.testclient import TestClient
 
@@ -10,5 +12,20 @@ from merino.main import app
 
 @pytest.fixture(name="client")
 def fixture_test_client() -> TestClient:
-    """Return a FastAPI TestClient instance for use in test methods"""
+    """Return a FastAPI TestClient instance.
+
+    Note that this will NOT trigger event handlers for the app, see:
+    https://fastapi.tiangolo.com/advanced/testing-events/
+    """
     return TestClient(app)
+
+
+@pytest.fixture(name="client_with_events")
+def fixture_test_client_with_events() -> Iterator[TestClient]:
+    """Return a FastAPI TestClient instance.
+
+    This test client will trigger event handlers for the app, see:
+    https://fastapi.tiangolo.com/advanced/testing-events/
+    """
+    with TestClient(app) as client:
+        yield client
