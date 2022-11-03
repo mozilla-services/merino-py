fa64ae0153f27f75592d155e0a2dec1ca8134717
Raphael Pierzina <raphael@hackebrot.de>
2022-11-03 10:40:19 +0100

    f: add more type hints to unit/test_metrics.py

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -2,19 +2,20 @@
 
 import aiodogstatsd
 import pytest
+from pytest_mock import MockerFixture
 
 from merino.featureflags import FeatureFlags
 from merino.metrics import Client
 
 
 @pytest.fixture(name="statsd_mock")
-def fixture_statsd_mock(mocker):
+def fixture_statsd_mock(mocker: MockerFixture):
     """Return mock for the StatsD client."""
     return mocker.MagicMock(spec_set=aiodogstatsd.Client)
 
 
-@pytest.fixture(name="metric_client")
-def fixture_metric_client(statsd_mock):
+@pytest.fixture(name="metrics_client")
+def fixture_metrics_client(statsd_mock):
     """Return a metrics client."""
     return Client(
         statsd_client=statsd_mock,
@@ -22,13 +23,13 @@ def fixture_metric_client(statsd_mock):
     )
 
 
-def test_unsupported_method(metric_client: Client, statsd_mock):
+def test_unsupported_method(metrics_client: Client, statsd_mock):
     """Test that the metrics client raises an error for unsupported methods."""
 
     want = "attribute 'hello_world' is not supported by metrics.Client class"
 
     with pytest.raises(AttributeError) as exc_info:
-        metric_client.hello_world(123, "abc")
+        metrics_client.hello_world(123, "abc")
 
     # Verify that the exception message is what we expect
     assert str(exc_info.value) == want
