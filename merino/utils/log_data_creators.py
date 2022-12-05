5c8b5f23b52d0793f188be2e3667325e76368c33
Michelle Tran <mitran@mozilla.com>
2022-12-05 16:01:32 -0500

    Put the log metadata fields into their own objects
    This will make it easier to do data validation on fields we send to logger
    and also encapsulates the logging metadata into extendable objects.
    
    Fixes DISCO-2044

--- merino/utils/log_data_creators.py
+++ merino/utils/log_data_creators.py
@@ -1,7 +1,8 @@
 """A utility module for lag data creation"""
 from datetime import datetime
-from typing import Any
+from typing import Any, Optional
 
+from pydantic import BaseModel
 from starlette.datastructures import Headers
 from starlette.requests import Request
 from starlette.types import Message
@@ -11,63 +12,98 @@ from merino.middleware.geolocation import Location
 from merino.middleware.user_agent import UserAgent
 
 
+class LogDataModel(BaseModel):
+    """Shared generic log data model. These fields are shared between the Request Summary Logs
+    and the Suggest Logs.
+    """
+
+    errno: int
+    time: datetime
+    path: str
+    method: str
+
+    def dict(self, **kwargs) -> dict[str, Any]:
+        """Override the dict method to convert the datetime type to iso-formatted string."""
+        d: dict[str, Any] = super().dict(**kwargs)
+        if d.get("time"):
+            d["time"] = d["time"].isoformat()
+        return d
+
+
+class RequestSummaryLogDataModel(LogDataModel):
+    """Log metadata specific to Request Summary."""
+
+    agent: Optional[str] = None
+    lang: Optional[str] = None
+    querystring: dict[str, Any]
+    code: int
+
+
+class SuggestLogDataModel(LogDataModel):
+    """Log metadata specific to Suggest logs."""
+
+    sensitive: bool
+    query: Optional[str] = None
+    code: int
+    rid: str  # Provided by the asgi-correlation-id middleware.
+    session_id: Optional[str] = None
+    sequence_no: Optional[int] = None
+    client_variants: str
+    requested_providers: str
+    country: Optional[str] = None
+    region: Optional[str] = None
+    city: Optional[str] = None
+    dma: Optional[int] = None
+    browser: str
+    os_family: str
+    form_factor: str
+
+
 def create_request_summary_log_data(
     request: Request, message: Message, dt: datetime
-) -> dict[str, Any]:
+) -> RequestSummaryLogDataModel:
     """Create log data for API endpoints."""
-    general_data = {
-        "errno": 0,
-        "time": dt.isoformat(),
-    }
-
-    request_data = {
-        "agent": request.headers.get("User-Agent"),
-        "path": request.url.path,
-        "method": request.method,
-        "lang": request.headers.get("Accept-Language"),
-        "querystring": dict(request.query_params),
-        "code": message["status"],
-    }
-
-    return {**general_data, **request_data}
+    return RequestSummaryLogDataModel(
+        errno=0,
+        time=dt,
+        agent=request.headers.get("User-Agent"),
+        path=request.url.path,
+        method=request.method,
+        lang=request.headers.get("Accept-Language"),
+        querystring=dict(request.query_params),
+        code=message["status"],
+    )
 
 
 def create_suggest_log_data(
     request: Request, message: Message, dt: datetime
-) -> dict[str, Any]:
+) -> SuggestLogDataModel:
     """Create log data for the suggest API endpoint."""
-    general_data = {
-        "sensitive": True,
-        "errno": 0,
-        "time": dt.isoformat(),
-    }
-
-    request_data = {
-        "path": request.url.path,
-        "method": request.method,
-        "query": request.query_params.get("q"),
-        "code": message["status"],
-        # Provided by the asgi-correlation-id middleware.
-        "rid": Headers(scope=message)["X-Request-ID"],
-        "session_id": request.query_params.get("sid"),
-        "sequence_no": int(seq) if (seq := request.query_params.get("seq")) else None,
-        "client_variants": request.query_params.get("client_variants", ""),
-        "requested_providers": request.query_params.get("providers", ""),
-    }
-
     location: Location = request.scope[ScopeKey.GEOLOCATION]
-    location_data = {
-        "country": location.country,
-        "region": location.region,
-        "city": location.city,
-        "dma": location.dma,
-    }
-
     user_agent: UserAgent = request.scope[ScopeKey.USER_AGENT]
-    user_agent_data = {
-        "browser": user_agent.browser,
-        "os_family": user_agent.os_family,
-        "form_factor": user_agent.form_factor,
-    }
 
-    return {**general_data, **request_data, **location_data, **user_agent_data}
+    return SuggestLogDataModel(
+        # General Data
+        sensitive=True,
+        errno=0,
+        time=dt,
+        # Request Data
+        path=request.url.path,
+        method=request.method,
+        query=request.query_params.get("q"),
+        code=message["status"],
+        rid=Headers(scope=message)["X-Request-ID"],
+        session_id=request.query_params.get("sid"),
+        sequence_no=int(seq) if (seq := request.query_params.get("seq")) else None,
+        client_variants=request.query_params.get("client_variants", ""),
+        requested_providers=request.query_params.get("providers", ""),
+        # Location Data
+        country=location.country,
+        region=location.region,
+        city=location.city,
+        dma=location.dma,
+        # User Agent Data
+        browser=user_agent.browser,
+        os_family=user_agent.os_family,
+        form_factor=user_agent.form_factor,
+    )
