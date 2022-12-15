9033df475e663af83a90950a4f54e47bb78560ca
Nan Jiang <njiang028@gmail.com>
2022-12-14 20:50:26 -0500

    fix reviews

--- tests/integration/api/v1/suggest/test_timeout_handling.py
+++ tests/integration/api/v1/suggest/test_timeout_handling.py
@@ -5,6 +5,7 @@
 """Integration tests for the Merino v1 suggest API endpoint focusing on time out
 behavior.
 """
+from collections import namedtuple
 
 import aiodogstatsd
 import pytest
@@ -19,130 +20,137 @@ from tests.integration.api.v1.fake_providers import (
 )
 from tests.types import FilterCaplogFixture
 
-
-@pytest.mark.parametrize(
+Scenario = namedtuple(
+    "Scenario",
     [
         "providers",
         "expected_suggestion_count",
         "expected_logs_on_task_runner",
         "expected_metric_keys",
     ],
-    [
-        # Case I: Only one provider that will time out.
-        #
-        #   - Expects:
-        #     - No suggestions
-        #     - Timeout logs recorded in the task runner
-        #     - Timeout metrics recorded in the task runner
-        (
-            {
-                "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
-            },
-            0,
-            {
-                "Timeout triggered in the task runner",
-                "Cancelling the task: timedout-sponsored due to timeout",
-            },
-            {
-                "providers.timedout-sponsored.query",
-                "providers.timedout-sponsored.query.timeout",
-                "suggestions-per.request",
-                "suggestions-per.provider.timedout-sponsored",
-                "get.api.v1.suggest.timing",
-                "get.api.v1.suggest.status_codes.200",
-                "response.status_codes.200",
-            },
-        ),
-        # Case II: One provider that will time out and another one will not.
-        #
-        #   - Expects:
-        #     - 1 suggestion returned from the non-timedout provider
-        #     - Timeout logs recorded in the task runner
-        #     - Timeout metrics recorded in the task runner
-        (
-            {
-                "sponsored": SponsoredProvider(enabled_by_default=True),
-                "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
-            },
-            1,
-            {
-                "Timeout triggered in the task runner",
-                "Cancelling the task: timedout-sponsored due to timeout",
-            },
-            {
-                "providers.sponsored.query",
-                "providers.timedout-sponsored.query",
-                "providers.timedout-sponsored.query.timeout",
-                "suggestions-per.request",
-                "suggestions-per.provider.sponsored",
-                "suggestions-per.provider.timedout-sponsored",
-                "get.api.v1.suggest.timing",
-                "get.api.v1.suggest.status_codes.200",
-                "response.status_codes.200",
-            },
-        ),
-        # Case III: One provider that should not time out since it uses the custom timeout
-        #           which is larger than its query duration.
-        #
-        #   - Expects:
-        #     - 1 suggestion returned from this provider
-        #     - Timeout logs should not be recorded in the task runner
-        #     - Timeout metrics should not be recorded in the task runner
-        (
-            {
-                "timedout-tolerant-sponsored": TimeoutTolerantSponsoredProvider(
-                    enabled_by_default=True
-                ),
-            },
-            1,
-            set(),
-            {
-                "providers.timedout-tolerant-sponsored.query",
-                "suggestions-per.request",
-                "suggestions-per.provider.timedout-tolerant-sponsored",
-                "get.api.v1.suggest.timing",
-                "get.api.v1.suggest.status_codes.200",
-                "response.status_codes.200",
-            },
-        ),
-        # Case IV: A regular non-timedout provider, a timed-out-tolerant provider, and a timed-out
-        #          provider.
-        #
-        #   - Expects:
-        #     - 3 suggestions returned from all three providers since the max timeout is overridden
-        #       by the timed-out-tolerant provider
-        #     - Timeout logs should not be recorded in the task runner
-        #     - Timeout metrics should not be recorded in the task runner
-        (
-            {
-                "sponsored": SponsoredProvider(enabled_by_default=True),
-                "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
-                "timedout-tolerant-sponsored": TimeoutTolerantSponsoredProvider(
-                    enabled_by_default=True
-                ),
-            },
-            3,
-            set(),
-            {
-                "providers.sponsored.query",
-                "providers.timedout-sponsored.query",
-                "providers.timedout-tolerant-sponsored.query",
-                "suggestions-per.request",
-                "suggestions-per.provider.timedout-tolerant-sponsored",
-                "suggestions-per.provider.timedout-sponsored",
-                "suggestions-per.provider.sponsored",
-                "get.api.v1.suggest.timing",
-                "get.api.v1.suggest.status_codes.200",
-                "response.status_codes.200",
-            },
-        ),
+)
+
+SCENARIOS: dict[str, Scenario] = {
+    # Case I: Only one provider that will time out.
+    #
+    #   - Expects:
+    #     - No suggestions
+    #     - Timeout logs recorded in the task runner
+    #     - Timeout metrics recorded in the task runner
+    "Case-I: A-timed-out-provider": Scenario(
+        providers={
+            "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
+        },
+        expected_suggestion_count=0,
+        expected_logs_on_task_runner={
+            "Timeout triggered in the task runner",
+            "Cancelling the task: timedout-sponsored due to timeout",
+        },
+        expected_metric_keys={
+            "providers.timedout-sponsored.query",
+            "providers.timedout-sponsored.query.timeout",
+            "suggestions-per.request",
+            "suggestions-per.provider.timedout-sponsored",
+            "get.api.v1.suggest.timing",
+            "get.api.v1.suggest.status_codes.200",
+            "response.status_codes.200",
+        },
+    ),
+    # Case II: One provider that will time out and another one will not.
+    #
+    #   - Expects:
+    #     - 1 suggestion returned from the non-timedout provider
+    #     - Timeout logs recorded in the task runner
+    #     - Timeout metrics recorded in the task runner
+    "Case-II: A-non-timed-out-and-a-timed-out-providers": Scenario(
+        providers={
+            "sponsored": SponsoredProvider(enabled_by_default=True),
+            "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
+        },
+        expected_suggestion_count=1,
+        expected_logs_on_task_runner={
+            "Timeout triggered in the task runner",
+            "Cancelling the task: timedout-sponsored due to timeout",
+        },
+        expected_metric_keys={
+            "providers.sponsored.query",
+            "providers.timedout-sponsored.query",
+            "providers.timedout-sponsored.query.timeout",
+            "suggestions-per.request",
+            "suggestions-per.provider.sponsored",
+            "suggestions-per.provider.timedout-sponsored",
+            "get.api.v1.suggest.timing",
+            "get.api.v1.suggest.status_codes.200",
+            "response.status_codes.200",
+        },
+    ),
+    # Case III: One provider that should not time out since it uses the custom timeout
+    #           which is larger than its query duration.
+    #
+    #   - Expects:
+    #     - 1 suggestion returned from this provider
+    #     - Timeout logs should not be recorded in the task runner
+    #     - Timeout metrics should not be recorded in the task runner
+    "Case-III: A-timed-out-tolerant-provider": Scenario(
+        providers={
+            "timedout-tolerant-sponsored": TimeoutTolerantSponsoredProvider(
+                enabled_by_default=True
+            ),
+        },
+        expected_suggestion_count=1,
+        expected_logs_on_task_runner=set(),
+        expected_metric_keys={
+            "providers.timedout-tolerant-sponsored.query",
+            "suggestions-per.request",
+            "suggestions-per.provider.timedout-tolerant-sponsored",
+            "get.api.v1.suggest.timing",
+            "get.api.v1.suggest.status_codes.200",
+            "response.status_codes.200",
+        },
+    ),
+    # Case IV: A regular non-timedout provider, a timed-out-tolerant provider, and a timed-out
+    #          provider.
+    #
+    #   - Expects:
+    #     - 3 suggestions returned from all three providers since the max timeout is overridden
+    #       by the timed-out-tolerant provider
+    #     - Timeout logs should not be recorded in the task runner
+    #     - Timeout metrics should not be recorded in the task runner
+    "Case-IV: A-non-timed-out-and-a-timed-out-tolerant-and-a-timed-out-providers": Scenario(
+        providers={
+            "sponsored": SponsoredProvider(enabled_by_default=True),
+            "timedout-sponsored": TimeoutSponsoredProvider(enabled_by_default=True),
+            "timedout-tolerant-sponsored": TimeoutTolerantSponsoredProvider(
+                enabled_by_default=True
+            ),
+        },
+        expected_suggestion_count=3,
+        expected_logs_on_task_runner=set(),
+        expected_metric_keys={
+            "providers.sponsored.query",
+            "providers.timedout-sponsored.query",
+            "providers.timedout-tolerant-sponsored.query",
+            "suggestions-per.request",
+            "suggestions-per.provider.timedout-tolerant-sponsored",
+            "suggestions-per.provider.timedout-sponsored",
+            "suggestions-per.provider.sponsored",
+            "get.api.v1.suggest.timing",
+            "get.api.v1.suggest.status_codes.200",
+            "response.status_codes.200",
+        },
+    ),
+}
+
+
+@pytest.mark.parametrize(
+    argnames=[
+        "providers",
+        "expected_suggestion_count",
+        "expected_logs_on_task_runner",
+        "expected_metric_keys",
     ],
-    ids=[
-        "Case-I: A-timed-out-provider",
-        "Case-II: A-non-timed-out-and-a-timed-out-providers",
-        "Case-III: A-timed-out-tolerant-provider",
-        "Case-IV: A-non-timed-out-and-a-timed-out-tolerant-and-a-timed-out-providers",
-    ],
+    argvalues=SCENARIOS.values(),
+    ids=SCENARIOS.keys(),
 )
 def test_timedout_providers(
     mocker: MockerFixture,
