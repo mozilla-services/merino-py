d62ff4cca8a87d1cca6fd2dd10886a3d08e3384e
Lina Butler <lina@yakshaving.ninja>
2023-02-16 13:05:36 -0800

    test: add new unit tests for cached weather suggestions

--- tests/unit/providers/weather/backends/test_accuweather.py
+++ tests/unit/providers/weather/backends/test_accuweather.py
@@ -787,3 +787,37 @@ async def test_get_forecast_error(
         await accuweather.get_forecast(mock_client, location_key)
 
     assert str(accuweather_error.value) == expected_error_value
+
+
+@pytest.mark.parametrize(
+    "cache_inputs_by_location",
+    [
+        (Location(country="US", region="CA", city="San Francisco", dma=807), None),
+        (
+            Location(region="CA", city="San Francisco", dma=807, postal_code="94105"),
+            None,
+        ),
+        (
+            Location(
+                country="US",
+                region="CA",
+                city="San Francisco",
+                dma=807,
+                postal_code="94105",
+            ),
+            b"US94105",
+        ),
+    ],
+)
+@pytest.mark.asyncio
+async def test_cache_inputs_for_weather_report(
+    accuweather: AccuweatherBackend,
+    cache_inputs_by_location: tuple[Location, Optional[bytes]],
+) -> None:
+    """Test that `cache_inputs_for_weather_report` computes the correct cache inputs when
+    given locations with various missing fields.
+    """
+    cache_inputs: Optional[bytes] = accuweather.cache_inputs_for_weather_report(
+        cache_inputs_by_location[0]
+    )
+    assert cache_inputs == cache_inputs_by_location[1]
