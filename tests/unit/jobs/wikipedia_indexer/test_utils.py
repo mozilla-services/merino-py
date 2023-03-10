682ed953bb758ec3ee61938e9b54176941cf1bff
Michelle Tran <email@michtran.ca>
2023-03-10 09:33:54 -0500

    Allow Indexer job to configure URL Elasticsearch connection (#231)
    This change will allow us to use the Elasticsearch URL if it is configured.
    If it doesn't exist, then fallback to using the Cloud ID.

--- tests/unit/jobs/wikipedia_indexer/test_utils.py
+++ tests/unit/jobs/wikipedia_indexer/test_utils.py
@@ -2,11 +2,18 @@
 
 
 import logging
+from unittest.mock import ANY
 
 import pytest
+from elasticsearch import Elasticsearch
 from pytest import LogCaptureFixture
+from pytest_mock import MockerFixture
 
-from merino.jobs.wikipedia_indexer.util import ProgressReporter, create_blocklist
+from merino.jobs.wikipedia_indexer.util import (
+    ProgressReporter,
+    create_blocklist,
+    create_elasticsearch_client,
+)
 
 
 @pytest.fixture()
@@ -64,3 +71,43 @@ def test_progress_reporter(
         else:
             assert len(caplog.records) == 1
             assert caplog.records[0].message == f"Test progress: {expected_perc}%"
+
+
+def test_create_elasticsearch_client_url(mocker: MockerFixture):
+    """Test that Elasticsearch client is created with the URL."""
+    es_new_mock = mocker.patch.object(Elasticsearch, "__new__")
+
+    api_key = "mMyY2E2ZDA0MjI0OWFmMGNjN2Q3YTllOTYyNTc0Mw=="
+    url = "http://localhost:9200"
+
+    # Have the cloud id for checking that it doesn't get called with this.
+    cloud_id = (
+        "testing:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyRjZ"
+        "WM2ZjI2MWE3NGJmMjRjZTMzYmI4ODExYjg0Mjk0Zi"
+        "RjNmMyY2E2ZDA0MjI0OWFmMGNjN2Q3YTllOTYyNTc0Mw=="
+    )
+
+    create_elasticsearch_client(url, cloud_id, api_key)
+
+    # First argument is "self", which I've stubbed with ANY
+    es_new_mock.assert_called_with(ANY, url, api_key=api_key, request_timeout=60)
+
+
+def test_create_elasticsearch_client_cloud_id(mocker: MockerFixture):
+    """Test that Elasticsearch client is created with the Cloud."""
+    es_new_mock = mocker.patch.object(Elasticsearch, "__new__")
+
+    api_key = "mMyY2E2ZDA0MjI0OWFmMGNjN2Q3YTllOTYyNTc0Mw=="
+    url = ""
+    cloud_id = (
+        "testing:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyRjZ"
+        "WM2ZjI2MWE3NGJmMjRjZTMzYmI4ODExYjg0Mjk0Zi"
+        "RjNmMyY2E2ZDA0MjI0OWFmMGNjN2Q3YTllOTYyNTc0Mw=="
+    )
+
+    create_elasticsearch_client(url, cloud_id, api_key)
+
+    # First argument is "self", which I've stubbed with ANY
+    es_new_mock.assert_called_with(
+        ANY, cloud_id=cloud_id, api_key=api_key, request_timeout=60
+    )
