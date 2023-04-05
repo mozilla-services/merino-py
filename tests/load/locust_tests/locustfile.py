18771d22ae5be096f5e1bf39306dcb8574794be6
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2023-04-05 13:51:38 -0400

    [DISCO-2167] Expand Merino-py load test to include Dynamic Wikipedia [load test: warn] (#233)
    * test: replace wikipedia query source with elasticsearch [load test: warn]
    
    * docs: update link to Merino configurations

--- tests/load/locust_tests/locustfile.py
+++ tests/load/locust_tests/locustfile.py
@@ -8,9 +8,10 @@ import asyncio
 import logging
 import os
 from random import choice, randint
-from typing import Any, Tuple
+from typing import Any
 
 import faker
+from elasticsearch import ApiError, Elasticsearch, ElasticsearchWarning, TransportError
 from locust import HttpUser, events, task
 from locust.clients import HttpSession
 from locust.runners import MasterRunner
@@ -42,8 +43,26 @@ SUGGEST_API: str = "/api/v1/suggest"
 # affecting the client's Suggest experience
 CLIENT_VARIANTS: str = ""
 
-# See Ops - Remote Settings
-# https://github.com/mozilla-services/merino-py/blob/main/docs/ops.md#remote_settings
+# Configurations
+# See Configuring Merino (https://mozilla-services.github.io/merino/ops.html)
+MERINO_PROVIDERS__TOP_PICKS__TOP_PICKS_FILE_PATH: str | None = os.getenv(
+    "MERINO_PROVIDERS__TOP_PICKS__TOP_PICKS_FILE_PATH"
+)
+MERINO_PROVIDERS__TOP_PICKS__QUERY_CHAR_LIMIT: int = int(
+    os.getenv("MERINO_PROVIDERS__TOP_PICKS__QUERY_CHAR_LIMIT", 0)
+)
+MERINO_PROVIDERS__TOP_PICKS__FIREFOX_CHAR_LIMIT: int = int(
+    os.getenv("MERINO_PROVIDERS__TOP_PICKS__FIREFOX_CHAR_LIMIT", 0)
+)
+MERINO_PROVIDERS__WIKIPEDIA__ES_URL: str | None = os.getenv(
+    "MERINO_PROVIDERS__WIKIPEDIA__ES_URL"
+)
+MERINO_PROVIDERS__WIKIPEDIA__ES_API_KEY: str | None = os.getenv(
+    "MERINO_PROVIDERS__WIKIPEDIA__ES_API_KEY"
+)
+MERINO_PROVIDERS__WIKIPEDIA__ES_INDEX: str | None = os.getenv(
+    "MERINO_PROVIDERS__WIKIPEDIA__ES_INDEX"
+)
 MERINO_REMOTE_SETTINGS__SERVER: str | None = os.getenv(
     "MERINO_REMOTE_SETTINGS__SERVER", os.getenv("KINTO__SERVER_URL")
 )
@@ -54,22 +73,11 @@ MERINO_REMOTE_SETTINGS__COLLECTION: str | None = os.getenv(
     "MERINO_REMOTE_SETTINGS__COLLECTION", os.getenv("KINTO__COLLECTION")
 )
 
-# See Ops - TOP PICKS
-# https://github.com/mozilla-services/merino-py/blob/main/docs/ops.md#top-picks-provider
-MERINO_PROVIDERS__TOP_PICKS__TOP_PICKS_FILE_PATH: str | None = os.getenv(
-    "MERINO_PROVIDERS__TOP_PICKS__TOP_PICKS_FILE_PATH"
-)
-MERINO_PROVIDERS__TOP_PICKS__QUERY_CHAR_LIMIT: int = int(
-    os.getenv("MERINO_PROVIDERS__TOP_PICKS__QUERY_CHAR_LIMIT", 0)
-)
-MERINO_PROVIDERS__TOP_PICKS__FIREFOX_CHAR_LIMIT: int = int(
-    os.getenv("MERINO_PROVIDERS__TOP_PICKS__FIREFOX_CHAR_LIMIT", 0)
-)
 
 # This will be populated on each worker
 ADM_QUERIES: QueriesList = []
 TOP_PICKS_QUERIES: QueriesList = []
-WIKIPEDIA_QUERIES: QueriesList = []
+WIKIPEDIA_QUERIES: list[str] = []
 
 
 @events.test_start.add_listener
@@ -80,14 +88,13 @@ def on_locust_test_start(environment, **kwargs):
 
     query_data: QueryData = QueryData()
     try:
-        query_data.adm, query_data.wikipedia = get_adm_queries(
+        query_data.adm = get_adm_queries(
             server=MERINO_REMOTE_SETTINGS__SERVER,
             collection=MERINO_REMOTE_SETTINGS__COLLECTION,
             bucket=MERINO_REMOTE_SETTINGS__BUCKET,
         )
 
         logger.info(f"Download {len(query_data.adm)} queries for AdM")
-        logger.info(f"Download {len(query_data.wikipedia)} queries for Wikipedia")
 
         query_data.top_picks = get_top_picks_queries(
             top_picks_file_path=MERINO_PROVIDERS__TOP_PICKS__TOP_PICKS_FILE_PATH,
@@ -97,7 +104,22 @@ def on_locust_test_start(environment, **kwargs):
 
         logger.info(f"Download {len(query_data.top_picks)} queries for Top Picks")
 
-    except (TopPicksError, RemoteSettingsError, ValueError):
+        query_data.wikipedia = get_wikipedia_queries(
+            url=MERINO_PROVIDERS__WIKIPEDIA__ES_URL,
+            api_key=MERINO_PROVIDERS__WIKIPEDIA__ES_API_KEY,
+            index=MERINO_PROVIDERS__WIKIPEDIA__ES_INDEX,
+        )
+
+        logger.info(f"Download {len(query_data.wikipedia)} queries for Wikipedia")
+
+    except (
+        ApiError,
+        ElasticsearchWarning,
+        RemoteSettingsError,
+        TopPicksError,
+        TransportError,
+        ValueError,
+    ):
         logger.error("Failed to gather query data. Stopping Test!")
         quit(1)
 
@@ -107,9 +129,7 @@ def on_locust_test_start(environment, **kwargs):
         )
 
 
-def get_adm_queries(
-    server: str, collection: str, bucket: str
-) -> Tuple[QueriesList, QueriesList]:
+def get_adm_queries(server: str, collection: str, bucket: str) -> QueriesList:
     """Get query strings for use in testing the AdM Provider.
 
     Args:
@@ -117,7 +137,7 @@ def get_adm_queries(
         collection: Kinto bucket with the suggestions
         bucket: Kinto collection with the suggestions
     Returns:
-        Tuple[QueriesList, QueriesList]: Lists of queries to use with the ADM provider
+        QueriesList: List of queries to use with the ADM provider
     Raises:
         ValueError: If 'server', 'collection' or 'bucket' parameters are None or
                     empty.
@@ -127,15 +147,10 @@ def get_adm_queries(
     content: SuggestionContent = asyncio.run(backend.fetch())
 
     adm_query_dict: dict[int, list[str]] = {}
-    wikipedia_query_dict: dict[int, list[str]] = {}
     for query, (result_id, fkw_index) in content.suggestions.items():
-        result: dict[str, Any] = content.results[result_id]
-        if result["advertiser"] == "Wikipedia":
-            wikipedia_query_dict.setdefault(result_id, []).append(query)
-        else:
-            adm_query_dict.setdefault(result_id, []).append(query)
+        adm_query_dict.setdefault(result_id, []).append(query)
 
-    return list(adm_query_dict.values()), list(wikipedia_query_dict.values())
+    return list(adm_query_dict.values())
 
 
 def get_top_picks_queries(
@@ -171,6 +186,36 @@ def get_top_picks_queries(
     add_queries(data.secondary_index, query_dict)
 
     return list(query_dict.values())
+
+
+def get_wikipedia_queries(
+    url: str | None, api_key: str | None, index: str | None
+) -> list[str]:
+    """Get query strings for use in testing the Wikipedia Provider.
+
+    Args:
+        url: The URL for the Elasticsearch cluster
+        api_key: The base64 key used to authenticate on the Elasticsearch cluster
+        index: A comma-separated list of index names to search; use `_all` or empty
+               string to perform the operation on all indices
+    Returns:
+        List[str]: List of full query strings to use with the Wikipedia provider
+    Raises:
+        ApiError: Error triggered from an HTTP response that isn’t 2XX
+        TransportError: Error triggered by an error occurring before an HTTP response
+                        arrives
+        ElasticsearchWarning: Warning that is raised when a deprecated option or
+                              incorrect usage is flagged via the ‘Warning’ HTTP header
+    """
+    with Elasticsearch(url, api_key=api_key) as client:
+        response = client.search(index=index, size=10000)  # maximum size
+
+    query_list: list[str] = []
+    for hit in response["hits"]["hits"]:
+        full_query: str = hit["_source"]["title"]
+        query_list.append(full_query)
+
+    return query_list
 
 
 def store_suggestions(environment, msg, **kwargs):
@@ -242,7 +287,7 @@ class QueryData(BaseModel):
 
     adm: QueriesList = []
     top_picks: QueriesList = []
-    wikipedia: QueriesList = []
+    wikipedia: list[str] = []
 
 
 class MerinoUser(HttpUser):
@@ -267,7 +312,7 @@ class MerinoUser(HttpUser):
     @task(weight=10)
     def adm_suggestions(self) -> None:
         """Send multiple requests for AdM queries."""
-        queries: list[str] = choice(ADM_QUERIES + WIKIPEDIA_QUERIES)  # nosec
+        queries: list[str] = choice(ADM_QUERIES)  # nosec
         providers: str = "adm"
 
         for query in queries:
@@ -276,10 +321,10 @@ class MerinoUser(HttpUser):
     @task(weight=10)
     def dynamic_wikipedia_suggestions(self) -> None:
         """Send multiple requests for Dynamic Wikipedia queries."""
-        # TODO Replace query source with ElasticSearch Source, not RemoteSettings
-        queries: list[str] = choice(WIKIPEDIA_QUERIES)  # nosec
+        full_query: str = choice(WIKIPEDIA_QUERIES)  # nosec
         providers: str = "wikipedia"
 
+        queries: list[str] = [full_query[:x] for x in range(2, (len(full_query) + 1))]
         for query in queries:
             request_suggestions(self.client, query, providers)
 
