7e54becb5783c0f27175664f8b7a80e903408ea4
Taddes Korris <tkorris@mozilla.com>
2023-04-21 12:10:49 -0400

    revision request changes

--- merino/config_sentry.py
+++ merino/config_sentry.py
@@ -1,15 +1,11 @@
 """Sentry Configuration"""
 
-import logging
-
 import sentry_sdk
 from sentry_sdk.integrations.fastapi import FastApiIntegration
 from sentry_sdk.integrations.starlette import StarletteIntegration
 
 from merino.config import settings
 from merino.utils.version import fetch_app_version_from_file
-
-logger = logging.getLogger(__name__)
 
 
 def configure_sentry() -> None:  # pragma: no cover
@@ -38,39 +34,29 @@ def configure_sentry() -> None:  # pragma: no cover
 def strip_sensitive_data(event: dict, hint: dict) -> dict:
     """Filter out sensitive data from Sentry events."""
     #  See: https://docs.sentry.io/platforms/python/configuration/filtering/
-    match event:
-        case {"request": {"query_string": _}}:
-            event["request"]["query_string"] = ""
-        case _:
-            pass
+    if event.get("request", {}).get("query_string", {}):
+        event["request"]["query_string"] = ""
 
     if "exc_info" in hint:
         exc_type, exc_value, tb = hint["exc_info"]
         if isinstance(exc_value, Exception):
-            try:
-                for entry in event["exception"]["values"][0]["stacktrace"]["frames"]:
-                    try:
-                        if entry["vars"].get("q"):
-                            entry["vars"]["q"] = ""
-                        if entry["vars"].get("query"):
-                            entry["vars"]["query"] = ""
-                        if entry["vars"].get("srequest"):
-                            entry["vars"]["srequest"] = ""
-                        if entry["vars"].get("values", {}).get("q"):
-                            entry["vars"]["values"]["q"] = ""
-                        if (
-                            entry["vars"].get("solved_result", [])
-                            and len(entry["vars"].get("solved_result", [])) > 0
-                        ):
-                            if entry["vars"]["solved_result"][0].get("q"):
-                                entry["vars"]["solved_result"][0]["q"] = ""
-                    except KeyError:
-                        continue
-
-            except KeyError as e:
-                logger.warning(
-                    f"Error during Sentry strip_sensitive_data callback: {e}"
-                )
-                pass
+            for entry in event["exception"]["values"][0]["stacktrace"]["frames"]:
+                try:
+                    if entry["vars"].get("q"):
+                        entry["vars"]["q"] = ""
+                    if entry["vars"].get("query"):
+                        entry["vars"]["query"] = ""
+                    if entry["vars"].get("srequest"):
+                        entry["vars"]["srequest"] = ""
+                    if entry["vars"].get("values", {}).get("q"):
+                        entry["vars"]["values"]["q"] = ""
+                    if (
+                        entry["vars"].get("solved_result", [])
+                        and len(entry["vars"].get("solved_result", [])) > 0
+                    ):
+                        if entry["vars"]["solved_result"][0].get("q"):
+                            entry["vars"]["solved_result"][0]["q"] = ""
+                except KeyError:
+                    continue
 
     return event
