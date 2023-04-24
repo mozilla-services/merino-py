04ed42e6f7eaba2eaab3471cbf479bd29f244d06
Taddes <tkorris@mozilla.com>
2023-04-24 15:52:54 -0400

    [DISCO-2235] Exclude Sensitive Data from Sentry  (#262)
    * initial commit, boilerplate function
    
    * testing boilerplate for sentry init
    
    * mock for processing sentry events
    
    * removal of named query attributes
    
    * completed filtering of q parameter from providers and tests for filtering
    
    * provider specific filtering
    
    * added case for provider specific q, query and srequest values
    
    * comment revisions, bug fix and discovery of 2 cases residing within one frame
    
    * revision request changes
    
    * test additions and addition of try except block
    
    * removed needless comment
    
    * docstring nit
    
    * removed check for hint exc value, left tests intact as hint always sent
    
    * added additional catch for IndexError
    
    * rename of test function

--- merino/config_sentry.py
+++ merino/config_sentry.py
@@ -1,4 +1,5 @@
 """Sentry Configuration"""
+import logging
 
 import sentry_sdk
 from sentry_sdk.integrations.fastapi import FastApiIntegration
@@ -6,6 +7,8 @@ from sentry_sdk.integrations.starlette import StarletteIntegration
 
 from merino.config import settings
 from merino.utils.version import fetch_app_version_from_file
+
+logger = logging.getLogger(__name__)
 
 
 def configure_sentry() -> None:  # pragma: no cover
@@ -22,9 +25,42 @@ def configure_sentry() -> None:  # pragma: no cover
         ],
         release=version_sha,
         debug="debug" == settings.sentry.mode,
+        before_send=strip_sensitive_data,
         environment=settings.sentry.env,
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
         traces_sample_rate=settings.sentry.traces_sample_rate,
     )
+
+
+def strip_sensitive_data(event: dict, hint: dict) -> dict:
+    """Filter out sensitive data from Sentry events."""
+    #  See: https://docs.sentry.io/platforms/python/configuration/filtering/
+    if event.get("request", {}).get("query_string", {}):
+        event["request"]["query_string"] = ""
+
+    try:
+        for entry in event["exception"]["values"][0]["stacktrace"]["frames"]:
+
+            if entry["vars"].get("q"):
+                entry["vars"]["q"] = ""
+            if entry["vars"].get("query"):
+                entry["vars"]["query"] = ""
+            if entry["vars"].get("srequest"):
+                entry["vars"]["srequest"] = ""
+            if entry["vars"].get("values", {}).get("q"):
+                entry["vars"]["values"]["q"] = ""
+            if (
+                entry["vars"].get("solved_result", [])
+                and len(entry["vars"].get("solved_result", [])) > 0
+            ):
+                if entry["vars"]["solved_result"][0].get("q"):
+                    entry["vars"]["solved_result"][0]["q"] = ""
+
+    except (KeyError, IndexError) as e:
+        logger.warning(
+            f"Encountered KeyError or IndexError for value {e} while filtering Sentry data."
+        )
+
+    return event
---
+++ tests/unit/test_config_sentry.py
@@ -0,0 +1,163 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Unit tests for the config_sentry.py module."""
+import logging
+
+from pytest import LogCaptureFixture
+
+from merino.config_sentry import strip_sensitive_data
+from tests.types import FilterCaplogFixture
+
+mock_sentry_hint: dict[str, list] = {"exc_info": [RuntimeError, RuntimeError(), None]}
+
+mock_sentry_event_data: dict = {
+    "request": {
+        "method": "GET",
+        "url": "http://localhost:8000/api/v1/suggest",
+        "query_string": "query_str_foo",
+    },
+    "exception": {
+        "values": [
+            {
+                "stacktrace": {
+                    "frames": [
+                        {
+                            "filename": "merino/web/api_v1.py",
+                            "module": "merino.web.api_v1",
+                            "vars": {
+                                "request": {
+                                    "type": "'http'",
+                                    "method": "'GET'",
+                                    "path": "'/api/v1/suggest'",
+                                },
+                                "q": "vars_foo",
+                                "providers": "'top_picks,adm'",
+                                "client_variants": "None",
+                            },
+                        },
+                        {
+                            "filename": "merino/web/api_v1.py",
+                            "module": "merino.web.api_v1",
+                            "vars": {
+                                "values": {
+                                    "request": {
+                                        "type": "'http'",
+                                        "method": "'GET'",
+                                        "path": "'/api/v1/suggest'",
+                                    },
+                                    "q": "vars_values_foo",
+                                    "providers": "'top_picks,adm'",
+                                    "client_variants": "None",
+                                }
+                            },
+                        },
+                        {
+                            "filename": "merino/providers/top_picks/provider.py",
+                            "function": "query",
+                            "context_line": "raise BackendError()",
+                            "vars": {
+                                "self": "<merino.providers.top_picks.provider>",
+                                "srequest": "SuggestionRequest(query='foobar')",
+                                "qlen": "6",
+                                "query": "foobar",
+                                "ids": "None",
+                            },
+                        },
+                        {
+                            "filename": "fastapi/routing.py",
+                            "function": "app",
+                            "module": "fastapi.routing",
+                            "lineno": 227,
+                            "vars": {
+                                "request": {
+                                    "type": "'http'",
+                                },
+                                "body": "None",
+                                "solved_result": [
+                                    {
+                                        "q": "foobar",
+                                        "providers": "'top_picks,adm'",
+                                        "client_variants": "None",
+                                        "request": {
+                                            "method": "'GET'",
+                                            "path": "'/api/v1/suggest'",
+                                        },
+                                    },
+                                ],
+                                "values": {
+                                    "sources": [
+                                        {
+                                            "accuweather": "<merino.providers.weather.provider>",
+                                            "adm": "<merino.providers.adm.provider.",
+                                            "top_picks": "<merino.providers.top_picks.provider>",
+                                            "wikipedia": "<merino.providers.wikipedia.provider>",
+                                        },
+                                    ],
+                                    "q": "'foobar'",
+                                    "providers": "'top_picks,adm'",
+                                    "client_variants": "None",
+                                },
+                            },
+                        },
+                    ]
+                },
+            }
+        ]
+    },
+}
+
+
+def test_strip_sensitive_data() -> None:
+    """Test that strip_sensitive_data will remove sensitive data."""
+    sanitized_event = strip_sensitive_data(mock_sentry_event_data, mock_sentry_hint)
+    assert sanitized_event["request"].get("query_string") == ""
+    assert "exc_info" in mock_sentry_hint
+    assert isinstance(mock_sentry_hint["exc_info"][1], RuntimeError)
+
+    assert (
+        sanitized_event["exception"]["values"][0]["stacktrace"]["frames"][0]["vars"][
+            "q"
+        ]
+        == ""
+    )
+    assert (
+        sanitized_event["exception"]["values"][0]["stacktrace"]["frames"][1]["vars"][
+            "values"
+        ]["q"]
+        == ""
+    )
+    assert (
+        sanitized_event["exception"]["values"][0]["stacktrace"]["frames"][2]["vars"][
+            "srequest"
+        ]
+        == ""
+    )
+
+    assert (
+        sanitized_event["exception"]["values"][0]["stacktrace"]["frames"][3]["vars"][
+            "solved_result"
+        ][0]["q"]
+        == ""
+    )
+
+
+def test_strip_sensitive_data_lookup_error(
+    caplog: LogCaptureFixture,
+    filter_caplog: FilterCaplogFixture,
+) -> None:
+    """Test that KeyError or IndexError message is emitted through logger when invalid key
+    or index is detected.
+    """
+    caplog.set_level(logging.WARNING)
+    strip_sensitive_data(
+        event={"bad_request": {}, "exception": {"invalid_values": [{}]}},
+        hint=mock_sentry_hint,
+    )
+
+    records = filter_caplog(caplog.records, "merino.config_sentry")
+    assert (
+        records[0].__dict__["msg"]
+        == "Encountered KeyError or IndexError for value 'values' while filtering Sentry data."
+    )
