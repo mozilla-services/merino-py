f4cd67c8333fd940a2534db0173fb953316bd230
Abhishek Aggarwal <aaggarwal@mozilla.com>
2023-06-06 14:45:45 +0200

    More comprehensive attributes for link and meta selectors for scraping favicons

--- merino/jobs/navigational_suggestions/domain_metadata_extractor.py
+++ merino/jobs/navigational_suggestions/domain_metadata_extractor.py
@@ -32,9 +32,12 @@ class Scraper:
     LINK_SELECTOR: str = (
         "link[rel=apple-touch-icon], link[rel=apple-touch-icon-precomposed],"
         'link[rel="icon shortcut"], link[rel="shortcut icon"], link[rel="icon"],'
-        'link[rel="SHORTCUT ICON"], link[rel="fluid-icon"]'
+        'link[rel="SHORTCUT ICON"], link[rel="fluid-icon"], link[rel="mask-icon"],'
+        'link[rel="apple-touch-startup-image"]'
     )
-    META_SELECTOR: str = "meta[name=apple-touch-icon]"
+    META_SELECTOR: str = (
+        "meta[name=apple-touch-icon], meta[name=msapplication-TileImage]"
+    )
 
     browser: RoboBrowser
 
