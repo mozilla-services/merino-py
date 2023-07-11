53b2459725880222850cddcf4e8422fe6f2a0bd4
Tif Tran <ttran@mozilla.com>
2023-07-10 21:59:05 -0700

    Update tests/integration/api/v1/suggest/test_suggest_weather.py
    Co-authored-by: Nan Jiang <najiang@mozilla.com>

--- tests/integration/api/v1/suggest/test_suggest_weather.py
+++ tests/integration/api/v1/suggest/test_suggest_weather.py
@@ -157,7 +157,7 @@ def test_providers_request_log_data_weather(
     extract_request_summary_log_data: RequestSummaryLogDataFixture,
     client: TestClient,
 ) -> None:
-    """Test that the request log for the 'accuweather' endpoint logs using request.summary"""
+    """Test that the request log for the "accuweather" provider logs using "request.summary" instead of "web.request.request""""
     caplog.set_level(logging.INFO)
 
     expected_log_data: RequestSummaryLogDataModel = RequestSummaryLogDataModel(
