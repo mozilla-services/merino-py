e2dfdc739989c905c0b9ad047ed8149c50ea5a16
Mathijs Miermans <mmiermans@users.noreply.github.com>
2024-07-17 12:44:12 +0200

    [load test: warn] [MC-1229] feat: add metrics to curated-recommendations (#567)
    * [MC-1229] feat: add metrics to curated-recommendations
    
    * Remove corpus_api_backend cache hits/misses
    
    I suspect that these metrics might be incorrectly interpreted,
    given that this backend always returns a cached value if it is
    available. A 100% cache hit rate wouldn't mean that the cache is
    never updated. The other metrics should get us the information we
    need.
    
    * Document metrics and logs
    
    * [MC-1265] docs: update for curated recommendations
    
    * implement review feedback
    
    - Fix test descriptions
    - Shorten metric names
    
    Additionally I organized tests in test_curated_recommendations.py into classes,
    to better be able to find tests.
    
    * fix: lint warning [annotation-unchecked]
    
    tests/integration/api/v1/curated_recommendations/test_curated_recommendations.py:125: note: By default the bodies of untyped functions are not checked, consider using --check-untyped-defs  [annotation-unchecked]
    
    * fix: poorly named variables in TestCuratedRecommendationsMetrics
    
    * fix: api doc link
    
    Co-authored-by: Nan Jiang <najiang@mozilla.com>
    
    ---------
    
    Co-authored-by: Nan Jiang <najiang@mozilla.com>

--- merino/curated_recommendations/corpus_backends/corpus_api_backend.py
+++ merino/curated_recommendations/corpus_backends/corpus_api_backend.py
@@ -6,6 +6,7 @@ import random
 from datetime import datetime, timedelta
 import asyncio
 
+import aiodogstatsd
 from httpx import AsyncClient, HTTPError
 
 from merino.curated_recommendations.corpus_backends.protocol import (
@@ -79,6 +80,7 @@ class CorpusApiBackend(CorpusBackend):
 
     http_client: AsyncClient
     graph_config: CorpusApiGraphConfig
+    metrics_client: aiodogstatsd.Client
 
     # time-to-live was chosen because 1 minute (+/- 10 s) is short enough that updates by curators
     # such as breaking news or editorial corrections propagate fast enough, and that the request
@@ -93,9 +95,15 @@ class CorpusApiBackend(CorpusBackend):
     _locks: dict[ScheduledSurfaceId, asyncio.Lock]
     _background_tasks: set[asyncio.Task]
 
-    def __init__(self, http_client: AsyncClient, graph_config: CorpusApiGraphConfig):
+    def __init__(
+        self,
+        http_client: AsyncClient,
+        graph_config: CorpusApiGraphConfig,
+        metrics_client: aiodogstatsd.Client,
+    ):
         self.http_client = http_client
         self.graph_config = graph_config
+        self.metrics_client = metrics_client
         self._cache = {}
         self._expirations = {}
         self._locks = {}
@@ -187,11 +195,14 @@ class CorpusApiBackend(CorpusBackend):
             },
         }
 
-        res = await self.http_client.post(
-            self.graph_config.endpoint,
-            json=body,
-            headers=self.graph_config.headers,
-        )
+        with self.metrics_client.timeit("corpus_api.request.timing"):
+            res = await self.http_client.post(
+                self.graph_config.endpoint,
+                json=body,
+                headers=self.graph_config.headers,
+            )
+
+        self.metrics_client.increment(f"corpus_api.request.status_codes.{res.status_code}")
 
         res.raise_for_status()
         data = res.json()
