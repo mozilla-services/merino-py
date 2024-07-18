c7fe7fa84b2901520d23928c50f99d5e61ce0984
Mathijs Miermans <m.miermans@gmail.com>
2024-07-18 21:17:03 +0200

    [MC-1352] feat: add tileId to curated-recommendations

--- tests/unit/curated_recommendations/test_provider.py
+++ tests/unit/curated_recommendations/test_provider.py
@@ -10,6 +10,8 @@ from merino.curated_recommendations.provider import (
     CuratedRecommendation,
     CuratedRecommendationsProvider,
     Locale,
+    MAX_TILE_ID,
+    MIN_TILE_ID,
 )
 
 
@@ -192,6 +194,7 @@ class TestCuratedRecommendationsProviderSpreadPublishers:
         recs = []
         for item_id in item_ids:
             rec = CuratedRecommendation(
+                tileId=MIN_TILE_ID + random.randint(0, 101),
                 receivedRank=random.randint(0, 101),
                 scheduledCorpusItemId=item_id,
                 url=HttpUrl("https://littlelarry.com/"),
@@ -338,3 +341,59 @@ class TestCuratedRecommendationsProviderSpreadPublishers:
             "7",
             "8",
         ]
+
+
+class TestCuratedRecommendationTileId:
+    """Unit tests for CuratedRecommendation tileId generation."""
+
+    # Common parameters for initializing CuratedRecommendation
+    common_params = {
+        "url": HttpUrl("https://example.com"),
+        "title": "Example Title",
+        "excerpt": "Example Excerpt",
+        "topic": Topic.CAREER,
+        "publisher": "Example Publisher",
+        "imageUrl": HttpUrl("https://example.com/image.jpg"),
+        "receivedRank": 1,
+    }
+
+    @pytest.mark.parametrize(
+        "scheduled_corpus_item_id, expected",
+        [
+            # Test random inputs. Boundary cases are not covered because sha256 is hard to reverse.
+            ("550e8400-e29b-41d4-a716-446655440000", 367820988390657),
+            ("6ba7b810-9dad-11d1-80b4-00c04fd430c8", 1754091520067902),
+            ("123e4567-e89b-12d3-a456-426614174000", 1021785982574447),
+            ("a3bb189e-8bf9-3888-9912-ace4e6543002", 4390412044299399),
+            ("c1a5fc62-9a4e-43f3-b748-2106a12e8151", 8630494423250594),
+        ],
+    )
+    def test_tile_id_generation(self, scheduled_corpus_item_id, expected):
+        """Testing the tile_id generation in the CuratedRecommendation constructor."""
+        # Create a CuratedRecommendation instance with the given scheduledCorpusItemId
+        recommendation = CuratedRecommendation(
+            scheduledCorpusItemId=scheduled_corpus_item_id,
+            **self.common_params,
+        )
+
+        assert recommendation.tileId == expected
+
+    @pytest.mark.parametrize("tile_id", [MIN_TILE_ID, MAX_TILE_ID])
+    def test_tile_id_min_max(self, tile_id):
+        """Test that the model can be initialized with MIN_TILE_ID and MAX_TILE_ID."""
+        recommendation = CuratedRecommendation(
+            scheduledCorpusItemId="550e8400-e29b-41d4-a716-446655440000",
+            tileId=tile_id,
+            **self.common_params,
+        )
+        assert recommendation.tileId == tile_id
+
+    @pytest.mark.parametrize("invalid_tile_id", [0, 999999, -1, (1 << 53)])
+    def test_invalid_tile_id(self, invalid_tile_id):
+        """Test that the model cannot be initialized with invalid tile IDs."""
+        with pytest.raises(ValueError):
+            CuratedRecommendation(
+                scheduledCorpusItemId="550e8400-e29b-41d4-a716-446655440000",
+                tileId=invalid_tile_id,
+                **self.common_params,
+            )
