babe34a91ffbc952d4e21833de3a222b07bc5cc0
Katerina Chinnappan <kchinnappan@mozilla.com>
2024-08-02 10:43:37 -0700

    [load test: warn]MC-1316: curated-recommendations load test with random topics (#592)
    * MC-1316: curated recs load test with random topics
    
    * address feedback
    
    * update task weight
    
    * update
    
    * make format
    
    * make format

--- tests/load/locustfiles/locustfile.py
+++ tests/load/locustfiles/locustfile.py
@@ -12,7 +12,7 @@ import os
 import socket
 import struct
 from itertools import chain
-from random import choice, randint
+from random import choice, randint, sample
 from typing import Any
 
 import faker
@@ -22,6 +22,7 @@ from locust.exception import StopUser
 from locust.runners import MasterRunner
 from pydantic import BaseModel
 
+from merino.curated_recommendations.corpus_backends.protocol import Topic
 from merino.providers.adm.backends.protocol import SuggestionContent
 from merino.providers.adm.backends.remotesettings import (
     RemoteSettingsBackend,
@@ -94,7 +95,6 @@ MERINO_REMOTE_SETTINGS__BUCKET: str | None = os.getenv(
 MERINO_REMOTE_SETTINGS__COLLECTION: str | None = os.getenv(
     "MERINO_REMOTE_SETTINGS__COLLECTION", os.getenv("KINTO__COLLECTION")
 )
-
 
 # This will be populated on each worker
 ADM_QUERIES: QueriesList = []
@@ -412,22 +412,44 @@ class MerinoUser(HttpUser):
 
         self._request_suggestions(query, providers, headers)
 
-    @task(weight=495)
-    def curated_recommendations(self) -> None:
-        """Send request to get curated recommendations."""
-        self._request_recommendations(CuratedRecommendationsRequest(locale=choice(list(Locale))))
+    @task(weight=421)
+    def curated_recommendations_locale(self) -> None:
+        """Send request to get curated recommendations, specifying random locale & 0 topics."""
+        self._request_recommendations(
+            CuratedRecommendationsRequest(locale=choice(list(Locale))), "locale"
+        )
 
-    def _request_recommendations(self, data: CuratedRecommendationsRequest) -> None:
+    @task(weight=74)
+    def curated_recommendations_random_topics(self) -> None:
+        """Send request to get curated recommendations with a random number of topics (between 1 & 4(max))."""
+        num_topics = randint(1, 4)  # Randomly choose between 1 and 4 topics
+        self._request_recommendations(
+            CuratedRecommendationsRequest(
+                locale=choice(list(Locale)),
+                topics=self.generate_random_arr_from_enum(
+                    enum_values=list(Topic), array_length=num_topics
+                ),
+            ),
+            "topics",
+        )
+
+    def _request_recommendations(
+        self,
+        data: CuratedRecommendationsRequest,
+        query_param: str | None = None,
+    ) -> None:
         """Request recommendations from Merino for the given data.
 
         Args:
             data: CuratedRecommendationsRequest object containing request data
+            query_param: query param passed in body request
         """
         with self.client.post(
             url=CURATED_RECOMMENDATIONS_API,
             json=data.model_dump(),
             headers={"User-Agent": choice(DESKTOP_FIREFOX)},
             catch_response=True,
+            name=f"{CURATED_RECOMMENDATIONS_API}{(f'?query_param={query_param}' if query_param else '')}",
         ) as response:
             if response.status_code == 0:
                 # Do not classify as failure
@@ -448,6 +470,11 @@ class MerinoUser(HttpUser):
             # from Merino. This will raise a ValidationError if the response is missing
             # fields which will be reported as a failure in Locust's statistics.
             CuratedRecommendationsResponse(**response.json())
+
+    @staticmethod
+    def generate_random_arr_from_enum(enum_values, array_length: int):
+        """Generate an array of random unique enum values."""
+        return sample(enum_values, array_length)
 
     @staticmethod
     def _get_ip_from_range(begin_ip_address: str, end_ip_address: str) -> str:
