a857cd32a6e63111363d6ff215d47e1f47fbd448
Katerina Chinnappan <kchinnappan@mozilla.com>
2024-11-04 17:38:13 +0100

    MC-1577: Final copy updates to Fakespot integration (#690)

--- merino/curated_recommendations/fakespot_backend/protocol.py
+++ merino/curated_recommendations/fakespot_backend/protocol.py
@@ -8,10 +8,10 @@ from merino.curated_recommendations.corpus_backends.protocol import ScheduledSur
 # Fakespot UI copy - hardcoded strings for now.
 FAKESPOT_DEFAULT_CATEGORY_NAME = "Holiday Gift Guide"
 FAKESPOT_HEADER_COPY = (
-    "Fakespot by Mozilla curates the chaos of online shopping into gift guides you can trust."
+    "Fakespot by Mozilla checks the reliability of product reviews so you don't have to."
 )
-FAKESPOT_FOOTER_COPY = "Take the guesswork out of gifting with the Fakespot Gift Guide."
-FAKESPOT_CTA_COPY = "Explore More Gifts"
+FAKESPOT_FOOTER_COPY = "Take the guesswork out of gifting with Fakespot's 2024 Gift Guide."
+FAKESPOT_CTA_COPY = "Explore more gifts"
 FAKESPOT_CTA_URL = "https://www.fakespot.com/giftguide/holidays2024/"
 # allowed locale for now
 FAKESPOT_CACHE_KEY = ScheduledSurfaceId.NEW_TAB_EN_US
