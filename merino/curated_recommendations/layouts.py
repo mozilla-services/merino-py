a478a7a406d6cd92d8b2b602fc3a5b76321ec8be
Katerina Chinnappan <kchinnappan@mozilla.com>
2024-11-13 11:55:32 -0800

    HNT-254: Adding hasExcerpt to Layouts (#706)
    * empty commit
    
    * HNT-254: adding hasExcerpt to layouts

--- merino/curated_recommendations/layouts.py
+++ merino/curated_recommendations/layouts.py
@@ -8,103 +8,103 @@ from merino.curated_recommendations.protocol import Layout, ResponsiveLayout, Ti
 # The following layouts are based on work-in-progress designs. The goal is to get some mock data to
 # FE developers. Names and layouts will likely be changed.
 
-# layout with 4 medium tiles, and an ad in 2nd position
+# layout with 4 medium tiles, an ad in 2nd position, and an excerpt in all tiles
 layout_4_medium = Layout(
     name="4-medium-tiles-ad-position-1",
     responsiveLayouts=[
         ResponsiveLayout(
             columnCount=4,
             tiles=[
-                Tile(size=TileSize.MEDIUM, position=0, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=1, hasAd=True),
-                Tile(size=TileSize.MEDIUM, position=2, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=3, hasAd=False),
+                Tile(size=TileSize.MEDIUM, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=1, hasAd=True, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=2, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=3, hasAd=False, hasExcerpt=True),
             ],
         ),
         ResponsiveLayout(
             columnCount=2,
             tiles=[
-                Tile(size=TileSize.MEDIUM, position=0, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=1, hasAd=True),
-                Tile(size=TileSize.MEDIUM, position=2, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=3, hasAd=False),
+                Tile(size=TileSize.MEDIUM, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=1, hasAd=True, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=2, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=3, hasAd=False, hasExcerpt=True),
             ],
         ),
         ResponsiveLayout(
             columnCount=1,
             tiles=[
-                Tile(size=TileSize.MEDIUM, position=0, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=1, hasAd=True),
-                Tile(size=TileSize.MEDIUM, position=2, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=3, hasAd=False),
+                Tile(size=TileSize.MEDIUM, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=1, hasAd=True, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=2, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=3, hasAd=False, hasExcerpt=True),
             ],
         ),
     ],
 )
 
-# layout with 4 small tiles, and no ads
+# layout with 4 small tiles, no ads and no excerpts
 layout_4_small = Layout(
     name="4-small-tiles-no-ads",
     responsiveLayouts=[
         ResponsiveLayout(
             columnCount=4,
             tiles=[
-                Tile(size=TileSize.SMALL, position=0, hasAd=False),
-                Tile(size=TileSize.SMALL, position=1, hasAd=False),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.SMALL, position=3, hasAd=False),
+                Tile(size=TileSize.SMALL, position=0, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=1, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=3, hasAd=False, hasExcerpt=False),
             ],
         ),
         ResponsiveLayout(
             columnCount=2,
             tiles=[
-                Tile(size=TileSize.SMALL, position=0, hasAd=False),
-                Tile(size=TileSize.SMALL, position=1, hasAd=False),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.SMALL, position=3, hasAd=False),
+                Tile(size=TileSize.SMALL, position=0, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=1, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=3, hasAd=False, hasExcerpt=False),
             ],
         ),
         ResponsiveLayout(
             columnCount=1,
             tiles=[
-                Tile(size=TileSize.SMALL, position=0, hasAd=False),
-                Tile(size=TileSize.SMALL, position=1, hasAd=False),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.SMALL, position=3, hasAd=False),
+                Tile(size=TileSize.SMALL, position=0, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=1, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=3, hasAd=False, hasExcerpt=False),
             ],
         ),
     ],
 )
 
-# layout with large-medium-small tiles, and an ad in medium tile
+# layout with large-medium-small tiles, an ad in medium tile, and an excerpt in a large tile and medium tile
 layout_large_medium_small = Layout(
     name="1-large-2-small-1-medium-ad-position-3",
     responsiveLayouts=[
         ResponsiveLayout(
             columnCount=4,
             tiles=[
-                Tile(size=TileSize.LARGE, position=0, hasAd=False),
-                Tile(size=TileSize.SMALL, position=1, hasAd=False),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=3, hasAd=True),
+                Tile(size=TileSize.LARGE, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.SMALL, position=1, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.MEDIUM, position=3, hasAd=True, hasExcerpt=True),
             ],
         ),
         ResponsiveLayout(
             columnCount=2,
             tiles=[
-                Tile(size=TileSize.LARGE, position=0, hasAd=False),
-                Tile(size=TileSize.SMALL, position=1, hasAd=False),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=3, hasAd=True),
+                Tile(size=TileSize.LARGE, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.SMALL, position=1, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.MEDIUM, position=3, hasAd=True, hasExcerpt=True),
             ],
         ),
         ResponsiveLayout(
             columnCount=1,
             tiles=[
-                Tile(size=TileSize.MEDIUM, position=0, hasAd=False),
-                Tile(size=TileSize.MEDIUM, position=1, hasAd=True),
-                Tile(size=TileSize.SMALL, position=2, hasAd=False),
-                Tile(size=TileSize.SMALL, position=3, hasAd=False),
+                Tile(size=TileSize.MEDIUM, position=0, hasAd=False, hasExcerpt=True),
+                Tile(size=TileSize.MEDIUM, position=1, hasAd=True, hasExcerpt=True),
+                Tile(size=TileSize.SMALL, position=2, hasAd=False, hasExcerpt=False),
+                Tile(size=TileSize.SMALL, position=3, hasAd=False, hasExcerpt=False),
             ],
         ),
     ],
