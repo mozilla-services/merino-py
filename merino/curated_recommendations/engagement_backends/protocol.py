84c55a90bdd11ef575b9c39045522351e005fcaf
Mathijs Miermans <m.miermans@gmail.com>
2025-01-06 18:37:09 +0100

    [MC-1608] Allow engagement to have a corpus_item_id

--- merino/curated_recommendations/engagement_backends/protocol.py
+++ merino/curated_recommendations/engagement_backends/protocol.py
@@ -7,14 +7,21 @@ from pydantic import BaseModel
 class Engagement(BaseModel):
     """Represents the engagement data from the last 24 hours for a scheduled corpus item.
 
-    Engagement is aggregated over a rolling 24-hour window by scheduled_corpus_item_id, a unique ID
-     for the URL, scheduled date, and surface (e.g., "New Tab en-US"). It's expected to be updated
+    Engagement is aggregated over a rolling 24-hour window by two different ids.
+    - scheduled_corpus_item_id, a unique ID for the URL, scheduled date, and surface (New Tab en-US)
+    - corpus_item_id, a unique ID only for the URL
+
+    Initially, each item had a scheduled_corpus_item_id, but in 2025Q1 we will start recommending
+    some items that only have a corpus_item_id.
+
+    It's expected to be updated
      periodically with a delay of < 30 minutes from when clicks or impressions happen in the client.
     Additionally, engagement data is aggregated for the regions US, CA, IE, GB, DE, AU, and CH.
      If region is None this means engagement data is aggregated across all regions.
     """
 
-    scheduled_corpus_item_id: str
+    scheduled_corpus_item_id: str | None = None
+    corpus_item_id: str | None = None  # Fx 135 started emitting corpus_item_id, so it may be null.
     region: str | None = None  # If region is None, then engagement is across all regions.
     click_count: int
     impression_count: int
