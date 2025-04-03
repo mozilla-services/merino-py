0f02c4469cdb6c81720f2d821466374153aba29e
Herraj Luhano <hluhano@mozilla.com>
2025-04-03 14:28:00 -0400

    [DISCO-3178] Replace robobrowser with mechanicalsoup (#877)
    * [DISCO-3178] Replace robobrowser with mechanicalsoup
    
    * remove unused import
    
    * add a close function and close the session after all icons are processed

--- merino/jobs/navigational_suggestions/domain_metadata_extractor.py
+++ merino/jobs/navigational_suggestions/domain_metadata_extractor.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin, urlparse
 
 import requests
 from pydantic import BaseModel
-from robobrowser import RoboBrowser
+from mechanicalsoup import StatefulBrowser
 
 from merino.jobs.navigational_suggestions.domain_metadata_uploader import DomainMetadataUploader
 from merino.utils.gcs.models import Image
@@ -40,14 +40,20 @@ class Scraper:
     )
     META_SELECTOR: str = "meta[name=apple-touch-icon], meta[name=msapplication-TileImage]"
     MANIFEST_SELECTOR: str = 'link[rel="manifest"]'
+    ALLOW_REDIRECTS = True
 
-    browser: RoboBrowser
+    browser: StatefulBrowser
     request_client: AsyncFaviconDownloader
+    parser: str = "html.parser"
 
     def __init__(self) -> None:
         session: requests.Session = requests.Session()
-        session.headers.update(REQUEST_HEADERS)
-        self.browser = RoboBrowser(session=session, parser="html.parser", allow_redirects=True)
+        self.browser = StatefulBrowser(
+            session=session,
+            soup_config={"features": self.parser},
+            raise_on_404=False,
+            user_agent=REQUEST_HEADERS["User-Agent"],
+        )
         self.request_client = AsyncFaviconDownloader()
 
     def open(self, url: str) -> Optional[str]:
@@ -59,7 +65,7 @@ class Scraper:
             Optional[str]: Full URL that was opened
         """
         try:
-            self.browser.open(url, timeout=TIMEOUT)
+            self.browser.open(url, timeout=TIMEOUT, allow_redirects=self.ALLOW_REDIRECTS)
             return str(self.browser.url)
         except Exception:
             return None
@@ -71,11 +77,11 @@ class Scraper:
             FaviconData: Favicon data for a URL
         """
         return FaviconData(
-            links=[link.attrs for link in self.browser.select(self.LINK_SELECTOR)],
-            metas=[meta.attrs for meta in self.browser.select(self.META_SELECTOR)],
+            links=[link.attrs for link in self.browser.page.select(self.LINK_SELECTOR)],
+            metas=[meta.attrs for meta in self.browser.page.select(self.META_SELECTOR)],
             manifests=[
                 manifest.attrs
-                for manifest in self.browser.select(f"head {self.MANIFEST_SELECTOR}")
+                for manifest in self.browser.page.select(f"head {self.MANIFEST_SELECTOR}")
             ],
         )
 
@@ -108,8 +114,8 @@ class Scraper:
         Returns:
             Optional[str]: Default favicon url if it exists
         """
+        default_favicon_url: str = urljoin(url, "favicon.ico")
         try:
-            default_favicon_url: str = urljoin(url, "favicon.ico")
             response = await self.request_client.requests_get(default_favicon_url)
             return str(response.url) if response else None
         except Exception:
@@ -122,8 +128,17 @@ class Scraper:
             Optional[str]: The title extracted from header of a url
         """
         try:
-            return str(self.browser.find("head").find("title").get_text())
-        except Exception:
+            return str(self.browser.page.find("head").find("title").get_text())
+        except Exception as e:
+            logger.info(f"Exception: {e} while scraping title")
+            return None
+
+    def close(self) -> None:
+        """Close the scraper browser session"""
+        try:
+            self.browser.close()
+        except Exception as ex:
+            logger.warning(f"Error ocurred when closing scraper session: {ex}")
             return None
 
 
@@ -481,6 +496,9 @@ class DomainMetadataExtractor:
                         logger.error(f"Unexpected result type: {result}")
                         continue
                     filtered_results.append(result)
+
+        # Close the scraper browser session
+        self.scraper.close()
 
         return filtered_results
 
