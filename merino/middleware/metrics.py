e6969d4cbb478e0073b579d134625318ba0e7331
Herraj Luhano <hluhano@mozilla.com>
2025-04-17 15:28:15 -0400

    [DISCO-3035] Refactor metrics middleware module

--- merino/middleware/metrics.py
+++ merino/middleware/metrics.py
@@ -1,12 +1,13 @@
-"""Middleware for request metrics."""
+"""Middleware for request metrics using FastAPI's middleware system."""
 
 import logging
-from asyncio import get_event_loop
-from functools import cache
 from http import HTTPStatus
+from time import monotonic
+from functools import cache
 
-from starlette.requests import Request
-from starlette.types import ASGIApp, Message, Receive, Scope, Send
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.responses import Response
 
 from merino.utils.metrics import get_metrics_client
 from merino.middleware import ScopeKey
@@ -14,66 +15,50 @@ from merino.middleware import ScopeKey
 logger = logging.getLogger(__name__)
 
 
-class MetricsMiddleware:
+class MetricsMiddleware(BaseHTTPMiddleware):
     """Middleware for instrumenting request level metrics. We currently collect timing
     and status codes for all known paths as well as status codes for all paths (known and unknown).
     """
 
-    def __init__(self, app: ASGIApp) -> None:
-        """Initialize the metrics middleware."""
-        self.app: ASGIApp = app
+    @cache
+    def _build_metric_name(self, method: str, path: str) -> str:
+        return "{}.{}".format(method, path.lower().lstrip("/").replace("/", ".")).lower()
 
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        """Wrap the request with metrics."""
-        if scope["type"] != "http":
-            await self.app(scope, receive, send)
-            return
-
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """Capture request metrics including timing and status codes."""
         # get the memoized StatsD client
         metrics_client = get_metrics_client()
 
         # Store `Client` instance in the request scope, so that it can be used by other
         # middleware and endpoints.
-        scope[ScopeKey.METRICS_CLIENT] = metrics_client
+        request.scope[ScopeKey.METRICS_CLIENT] = metrics_client
+        tags = request.scope[ScopeKey.USER_AGENT].model_dump()
 
-        loop = get_event_loop()
-        request = Request(scope=scope)
-        started_at = loop.time()
-        tags = scope[ScopeKey.USER_AGENT].model_dump()
-
-        async def send_wrapper(message: Message) -> None:
-            if message["type"] == "http.response.start":
-                duration = (loop.time() - started_at) * 1000
-
-                status_code = message["status"]
-                # don't track NOT_FOUND statuses by path.
-                # Instead we will track those within a general `response.status_codes` metric.
-                if status_code != HTTPStatus.NOT_FOUND.value:
-                    metric_name = self._build_metric_name(request.method, request.url.path)
-                    metrics_client.timing(
-                        f"{metric_name}.timing",
-                        value=duration,
-                    )
-                    metrics_client.increment(
-                        f"{metric_name}.status_codes.{status_code}", tags=tags
-                    )
-
-                # track all status codes here.
-                metrics_client.increment(f"response.status_codes.{status_code}", tags=tags)
-
-            await send(message)
-
+        # start the timer for request
+        started_at = monotonic()
         try:
-            await self.app(scope, receive, send_wrapper)
+            # pass request to the next middleware / request handler function and wait for response.
+            response = await call_next(request)
+
+            duration = (monotonic() - started_at) * 1000
+            status_code = response.status_code
+
+            # don't track NOT_FOUND statuses by path.
+            # Instead we will track those within a general `response.status_codes` metric.
+            if status_code != HTTPStatus.NOT_FOUND:
+                metric_name = self._build_metric_name(request.method, request.url.path)
+                metrics_client.timing(f"{metric_name}.timing", value=duration)
+                metrics_client.increment(f"{metric_name}.status_codes.{status_code}", tags=tags)
+
+            metrics_client.increment(f"response.status_codes.{status_code}", tags=tags)
+            return response
+
         except Exception:
-            status_code = HTTPStatus.INTERNAL_SERVER_ERROR.value
-            duration = (loop.time() - started_at) * 1000
+            duration = (monotonic() - started_at) * 1000
             metric_name = self._build_metric_name(request.method, request.url.path)
+            status_code = HTTPStatus.INTERNAL_SERVER_ERROR.value
+
             metrics_client.timing(f"{metric_name}.timing", value=duration, tags=tags)
             metrics_client.increment(f"{metric_name}.status_codes.{status_code}", tags=tags)
             metrics_client.increment(f"response.status_codes.{status_code}", tags=tags)
             raise
-
-    @cache
-    def _build_metric_name(self, method: str, path: str) -> str:
-        return "{}.{}".format(method, path.lower().lstrip("/").replace("/", ".")).lower()
