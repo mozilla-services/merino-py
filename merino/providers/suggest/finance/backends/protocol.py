4cc11382abb84caa63841a9adf00b7a09319683e
Herraj Luhano <hluhano@mozilla.com>
2025-05-26 15:52:25 -0400

    [DISCO-3542] Add skeleton work for finance provider

---
+++ merino/providers/suggest/finance/backends/protocol.py
@@ -0,0 +1,23 @@
+from typing import Protocol
+
+class FinanceBackend(Protocol):
+    """Protocol for a finance backend that this provider depends on.
+
+    Note: This only defines the methods used by the provider. The actual backend
+    might define additional methods and attributes which this provider doesn't
+    directly depend on.
+    """
+    # TODO
+    # async def get_something(
+    #     self, finance_context: FinanceContext
+    # ) -> FinanceReport | None:  # pragma: no cover
+    #     """Get finance information from partner.
+
+    #     Raises:
+    #         BackendError: Category of error specific to provider backends.
+    #     """
+    #     ...
+
+    async def shutdown(self) -> None:  # pragma: no cover
+        """Close down any open connections."""
+        ...
