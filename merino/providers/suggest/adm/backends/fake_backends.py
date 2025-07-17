cae48cff71d07dd3f0daeb9f3271a67e82560080
Tif Tran <ttran@mozilla.com>
2025-07-17 09:33:02 -0700

    [DISCO-3566] merino_ext integration

--- merino/providers/suggest/adm/backends/fake_backends.py
+++ merino/providers/suggest/adm/backends/fake_backends.py
@@ -1,11 +1,12 @@
 """Test backend for the AdM provider."""
 
-from merino.providers.suggest.adm.backends.protocol import SuggestionContent
+from merino.providers.suggest.adm.backends.protocol import SuggestionContentExt
+from moz_merino_ext.amp import AmpIndexManager
 
 
 class FakeAdmBackend:  # pragma: no cover
     """A fake backend that always returns empty results."""
 
-    async def fetch(self) -> SuggestionContent:
+    async def fetch(self) -> SuggestionContentExt:
         """Get fake Content from partner."""
-        return SuggestionContent()  # type: ignore [call-arg]
+        return SuggestionContentExt(index_manager=AmpIndexManager(), icons={})  # type: ignore[no-untyped-call]
