5dd85c64563fa90db4b31decb9c96d3ec552c2d0
Herraj Luhano <hluhano@mozilla.com>
2025-07-30 11:55:29 -0400

    fix comment

--- tests/unit/providers/suggest/finance/backends/test_utils.py
+++ tests/unit/providers/suggest/finance/backends/test_utils.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""Unit tests for the Polygon backend module."""
+"""Unit tests for the Polygon utils module."""
 
 import pytest
 from typing import Any
