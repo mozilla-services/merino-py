3da98b0e0a70c6b2648e41c6a9db4ab26c95e3ec
Herraj Luhano <hluhano@mozilla.com>
2025-08-06 18:10:27 -0400

    remove exports

--- merino/providers/suggest/finance/backends/polygon/__init__.py
+++ merino/providers/suggest/finance/backends/polygon/__init__.py
@@ -1,9 +1,5 @@
 """The backend for Polygon API interactions."""
 
 from merino.providers.suggest.finance.backends.polygon.backend import PolygonBackend
-from merino.providers.suggest.finance.backends.polygon.ticker_company_mapping import (
-    _TICKER_COMPANY,
-)
 
-
-__all__ = ["PolygonBackend", "_TICKER_COMPANY"]
+__all__ = ["PolygonBackend"]
