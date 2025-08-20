64e2bcf504be7dbfd233d18f715071d73c648d11
Temisan Iwere <tiwere@mozilla.com>
2025-08-20 14:24:31 -0400

    [DISCO-3656] Add skeleton for Yelp provider (#1036)
    * [DISCO-3656] Add skeleton for Yelp provider
    
    * fix typo
    
    * fix typos

---
+++ merino/providers/suggest/yelp/backends/protocol.py
@@ -0,0 +1,16 @@
+"""Protocol for yelp provider backends."""
+
+from typing import Protocol
+
+
+class YelpBackendProtocol(Protocol):
+    """Protocol for a yelp backend that this provider depends on.
+
+    Note: This only defines the methods used by the provider. The actual backend
+    might define additional methods and attributes which this provider doesn't
+    directly depend on.
+    """
+
+    async def shutdown(self) -> None:  # pragma: no cover
+        """Close down any open connections."""
+        ...
---
+++ merino/providers/suggest/yelp/provider.py
@@ -0,0 +1,47 @@
+"""Yelp Integration"""
+
+import aiodogstatsd
+from fastapi import HTTPException
+from merino.providers.suggest.base import BaseProvider, SuggestionRequest
+from merino.providers.suggest.yelp.backends.protocol import YelpBackendProtocol
+
+
+class Provider(BaseProvider):
+    """Suggestion provider for yelp"""
+
+    backend: YelpBackendProtocol
+    metrics_client: aiodogstatsd.Client
+    score: float
+
+    def __init__(
+        self,
+        backend: YelpBackendProtocol,
+        name: str,
+        metrics_client: aiodogstatsd.Client,
+        query_timeout_sec: float,
+        score: float,
+        enabled_by_default: bool = False,
+    ):
+        self.backend = backend
+        self.metrics_client = metrics_client
+        self.score = score
+        self._name = name
+        self._query_timeout_sec = query_timeout_sec
+        self._enabled_by_default = enabled_by_default
+        super().__init__()
+
+    async def initialize(self) -> None:
+        """Initialize the provider."""
+        # TODO
+
+    def validate(self, srequest: SuggestionRequest) -> None:
+        """Validate the suggestion request."""
+        if not srequest.query:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid query parameters: `q` is missing",
+            )
+
+    async def query(self, request: SuggestionRequest):
+        """Retrieve yelp suggestions"""
+        return []
