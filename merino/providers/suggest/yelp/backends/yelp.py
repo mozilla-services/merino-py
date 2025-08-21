99ca752d9c3729177efe57e539bc5c9f6c704e1d
Tif Tran <ttran@mozilla.com>
2025-08-21 12:55:10 -0700

    [DISCO-3658] Yelp intergration and response structure

--- merino/providers/suggest/yelp/backends/yelp.py
+++ merino/providers/suggest/yelp/backends/yelp.py
@@ -1,8 +1,14 @@
 """A wrapper for Yelp API interactions."""
 
-from httpx import AsyncClient
+import logging
+from typing import Any
+
+from httpx import AsyncClient, Response, HTTPStatusError
 
 from merino.providers.suggest.yelp.backends.protocol import YelpBackendProtocol
+
+LIMIT_DEFAULT = 1
+logger = logging.getLogger(__name__)
 
 
 class YelpBackend(YelpBackendProtocol):
@@ -10,13 +16,65 @@ class YelpBackend(YelpBackendProtocol):
 
     api_key: str
     http_client: AsyncClient
+    url_business_search: str
 
     def __init__(
         self,
         api_key: str,
+        http_client: AsyncClient,
+        url_business_search: str,
     ) -> None:
         """Initialize the Yelp backend."""
         self.api_key = api_key
+        self.http_client = http_client
+        self.url_business_search = url_business_search
+
+    async def get_yelp_businesses(self, search_term: str, location: str) -> dict | None:
+        """Get businesses from Yelp calling its api."""
+        headers = {"Authorization": f"Bearer {self.api_key}"}
+        params = {"term": search_term, "location": location, "limit": LIMIT_DEFAULT}
+        url = self.url_business_search.format(**params)
+        try:
+            response: Response = await self.http_client.get(url, headers=headers)
+            response.raise_for_status()
+            return self.proccess_response(response.json())
+        except HTTPStatusError as ex:
+            logger.warning(
+                f"Failed to get businesses for {search_term}/{location}: {ex.response.status_code} {ex.response.reason_phrase}"
+            )
+        return None
+
+    @staticmethod
+    def proccess_response(response: Any) -> dict | None:
+        """Process response from Yelp."""
+        match response:
+            case {
+                "businesses": [
+                    {
+                        "name": name,
+                        "url": url,
+                        "location": {"address1": address},
+                        "business_hours": business_hours,
+                    },
+                    *_,
+                ]
+            }:
+                # extract potentially null fields
+                business = response["businesses"][0]  # type: ignore[unreachable]
+                price = business.get("price")
+                rating = business.get("rating")
+                review_count = business.get("review_count")
+                return {
+                    "name": name,
+                    "url": url,
+                    "address": address,
+                    "rating": rating,
+                    "price": price,
+                    "review_count": review_count,
+                    "business_hours": business_hours,
+                }
+            case _:
+                return None
 
     async def shutdown(self) -> None:
         """Shutdown any persistent connections. Currently a no-op."""
--- merino/providers/suggest/yelp/provider.py
+++ merino/providers/suggest/yelp/provider.py
@@ -2,7 +2,10 @@
 
 import aiodogstatsd
 from fastapi import HTTPException
-from merino.providers.suggest.base import BaseProvider, SuggestionRequest
+from pydantic import HttpUrl
+
+from merino.providers.suggest.base import BaseProvider, SuggestionRequest, BaseSuggestion
+from merino.providers.suggest.custom_details import CustomDetails, YelpDetails
 from merino.providers.suggest.yelp.backends.protocol import YelpBackendProtocol
 
 
@@ -44,4 +47,30 @@ class Provider(BaseProvider):
 
     async def query(self, request: SuggestionRequest):
         """Retrieve yelp suggestions"""
+        search_term = self.process_query()
+        geolocation = request.geolocation
+        location = geolocation.city if geolocation else None
+        if not search_term or not location:
+            return []
+
+        yelp_business = await self.backend.get_yelp_businesses(search_term, location)
+        if yelp_business:
+            return [self.build_suggestion(yelp_business)]
         return []
+
+    def build_suggestion(self, data: dict) -> BaseSuggestion | None:
+        """Build the suggestion with custom yelp details."""
+        url = data.pop("url")
+        custom_details = CustomDetails(yelp=YelpDetails(**data))
+        return BaseSuggestion(
+            title="Yelp Suggestion",
+            url=HttpUrl(url),
+            provider=self.name,
+            is_sponsored=False,
+            score=0.26,  # need to confirm value
+            custom_details=custom_details,
+        )
+
+    def process_query(self) -> str:
+        """Stub placeholder until we finalize keywords."""
+        return "coffee"
