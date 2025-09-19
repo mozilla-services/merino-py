7a3b91fdc945510b66dbc10989b98e0b0246094b
Herraj Luhano <hluhano@mozilla.com>
2025-09-19 16:57:20 -0400

    [DISCO-3728] Improve ticker query keyword matching

--- merino/providers/suggest/finance/backends/polygon/utils.py
+++ merino/providers/suggest/finance/backends/polygon/utils.py
@@ -1,6 +1,7 @@
 """Utilities for the Polygon backend"""
 
 import logging
+import re
 from typing import Any
 
 from pydantic import HttpUrl
@@ -25,6 +26,11 @@ ALL_TICKER_COMPANY_MAPPING: dict[str, dict] = {
     **ALL_ETF_TICKER_COMPANY_MAPPING,
 }
 
+# This match either "stock(s) ABC" or "ABC stock(s)". Case insensitive.
+STOCK_QUERY_PATTERN = re.compile(
+    r"^(?:(?P<keyword1>\w+)\s+stocks?)$|^(?:stocks?\s+(?P<keyword2>\w+))$", re.IGNORECASE
+)
+
 
 def lookup_ticker_company(ticker: str) -> str:
     """Get the ticker company for a stock or ETF ticker symbol."""
@@ -36,22 +42,27 @@ def lookup_ticker_exchange(ticker: str) -> str:
     return str(ALL_TICKER_COMPANY_MAPPING[ticker]["exchange"])
 
 
-def get_tickers_for_query(keyword: str) -> list[str] | None:
+def get_tickers_for_query(query: str) -> list[str] | None:
     """Validate and return a list of tickers (1 to 3) or None."""
-    keyword_upper = keyword.upper()
+    query_upper = query.upper()
 
-    if (
-        keyword_upper in STOCK_TICKER_MATCH_BLOCKLIST
-        or keyword_upper in ETF_TICKER_MATCH_BLOCKLIST
-    ):
+    if stock_query := STOCK_QUERY_PATTERN.match(query_upper):
+        keyword = stock_query.group("keyword1") or stock_query.group("keyword2")
+        if keyword in ALL_STOCK_TICKER_COMPANY_MAPPING:
+            return [keyword]
+
+        if keyword in ALL_ETF_TICKER_COMPANY_MAPPING:
+            return [keyword]
+
+    if query_upper in STOCK_TICKER_MATCH_BLOCKLIST or query_upper in ETF_TICKER_MATCH_BLOCKLIST:
         return None
-    if keyword_upper in ALL_STOCK_TICKER_COMPANY_MAPPING:
-        return [keyword_upper]
-    if keyword_upper in ALL_ETF_TICKER_COMPANY_MAPPING:
-        return [keyword_upper]
-    if ticker := KEYWORD_TO_STOCK_TICKER_MAPPING.get(keyword):
+    if query_upper in ALL_STOCK_TICKER_COMPANY_MAPPING:
+        return [query_upper]
+    if query_upper in ALL_ETF_TICKER_COMPANY_MAPPING:
+        return [query_upper]
+    if ticker := KEYWORD_TO_STOCK_TICKER_MAPPING.get(query_upper):
         return [ticker]
-    if tickers := KEYWORD_TO_ETF_TICKER_MAPPING.get(keyword):
+    if tickers := KEYWORD_TO_ETF_TICKER_MAPPING.get(query_upper):
         return tickers
 
     return None
