54b8da790634ad10c37b594f21c14b18e2860040
Nan Jiang <njiang028@gmail.com>
2025-09-19 18:06:50 -0400

    [DISCO-3730] feat: Proxy requests for google suggest

--- merino/utils/http_client.py
+++ merino/utils/http_client.py
@@ -11,6 +11,7 @@ def create_http_client(
     connect_timeout: float = 1.0,
     request_timeout: float = 5.0,
     pool_timeout: float = 1.0,
+    proxies: dict[str, str] | None = None,
 ) -> AsyncClient:
     """Crete a new `httpx.AsyncClient` with common configurations.
 
@@ -20,6 +21,12 @@ def create_http_client(
       - `connect_timeout` {float}: The timeout for establishing a connection to the host.
       - `request_timeout` {float}: The timeuot for handling a request to the host.
       - `pool_timeout` {float}: The timeout for acquiring a connection from the pool.
+      - `proxies` {dict[str, str] | None}: A proxy dictionary for this client or no proxy is not set.
+        the dictionary should look like:
+        {
+            "http://", "http://{your-http-proxy.com}:{port}",
+            "https://", "http://{your-https-proxy.com}:{port}",
+        }
     Returns:
       - {AsyncClient}: An async HTTP client.
     """
@@ -27,4 +34,5 @@ def create_http_client(
         base_url=base_url,
         limits=Limits(max_connections=max_connections),
         timeout=Timeout(request_timeout, connect=connect_timeout, pool=pool_timeout),
+        proxies=proxies,  # type: ignore [arg-type]
     )
