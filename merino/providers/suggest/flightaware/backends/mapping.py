c623fc4c0c3b46aa9d05b3fb5c9e90414a6d60ea
Ali Patel <alpatel@mozilla.com>
2025-10-02 19:45:46 -0400

    [DISCO-3747] Linting fixes

--- merino/providers/suggest/flightaware/backends/mapping.py
+++ merino/providers/suggest/flightaware/backends/mapping.py
@@ -1,3 +1,5 @@
+"""Mapper to convert Name or ICAO to IATA with ICAO as backup"""
+
 import json
 
 # IATA code set
@@ -7,7 +9,7 @@ iata = set()
 name_mapping = {}
 icao_mapping = {}
 
-with open('airlines.json', 'r') as file:
+with open("airlines.json", "r") as file:
     data = json.load(file)
 
 for airline in data:
@@ -23,24 +25,25 @@ for airline in data:
 
 
 def parsing(query: str) -> str:
-    """Parses a query to an identified airline and flight number"""
+    """Parse a query to an identified airline and flight number"""
     query = query.strip().lower()
     idx = query.rfind(" ")
     if idx != "-1":
         name = query[:idx].strip()
         try:
             number = int(query[idx:].strip())
-        except:
+        except ValueError:
             return "Bad Number"
     else:
         return "No Number"
     if name.upper() in iata:
         return name.upper() + " " + str(number)
     elif name in name_mapping:
-        return name_mapping[name] + " " + str(number)
+        return str(name_mapping[name]) + " " + str(number)
     elif name.upper() in icao_mapping:
-        return icao_mapping[name] + " " + str(number)
+        return str(icao_mapping[name]) + " " + str(number)
     return "Unidentified Airline"
+
 
 # print(parsing("ac 130"))
 # print(parsing("united airlines 101"))
