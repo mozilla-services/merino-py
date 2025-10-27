579788f3c646fb1c1f6253f6a879d43468599efd
Rolf Rando <119353883+rolf-moz@users.noreply.github.com>
2025-10-27 10:59:42 -0700

    GENAI-2208 Rollout of crawled content as default for US (#1122)
    * Experiment setup for crawler rollout
    
    * Unit tests working
    
    * Fixed integration tests
    
    * fix
    
    * change default rescalar

--- tests/unit/prior_backends/test_experiment_rescaler.py
+++ tests/unit/prior_backends/test_experiment_rescaler.py
@@ -5,73 +5,72 @@ thompson sampling experiments
 from unittest.mock import Mock
 
 from merino.curated_recommendations.prior_backends.experiment_rescaler import (
-    SubsectionsExperimentRescaler,
-    CRAWLED_TOPIC_TOTAL_PERCENT,
-    CrawlerExperimentRescaler,
+    DefaultCrawlerRescaler,
+    SchedulerHoldbackRescaler,
     PESSIMISTIC_PRIOR_ALPHA_SCALE,
+    PESSIMISTIC_PRIOR_ALPHA_SCALE_SUBTOPIC,
 )
 
-SUBSECTION_EXPERIMENT_PERCENT = 0.25
+SECTIONS_HOLDBACK_TOTAL_PERCENT = 0.1
 SUBTOPIC_EXPERIMENT_CURATED_ITEM_FLAG = "in_subtopic_experiment"
 
 
-class TestSubsectionsExperimentRescaler:
+class TestDefaultRescaler:
     """Test Rig for the rescaler"""
 
     def setup_method(self):
         """Set up test"""
-        self.rescaler = SubsectionsExperimentRescaler(
-            subtopic_relative_size=SUBSECTION_EXPERIMENT_PERCENT
-        )
+        self.rescaler = DefaultCrawlerRescaler()
 
-    def test_rescale_when_in_experiment(self):
+    def test_rescale_with_subtopic_item(self):
         """Test rescaling of priors for relative experiment size"""
         rec = Mock()
-        rec.in_experiment.return_value = True
-        opens, no_opens = self.rescaler.rescale(rec, 100, 50)
-        expected_opens = 100 / SUBSECTION_EXPERIMENT_PERCENT
-        expected_no_opens = 50 / SUBSECTION_EXPERIMENT_PERCENT
+        rec.in_experiment.return_value = True  # Indicates subtopic for flag
+        rec.isTimeSensitive = False
+        expected_opens = 100
+        expected_no_opens = 50
+        opens, no_opens = self.rescaler.rescale(rec, expected_opens, expected_no_opens)
         assert opens == expected_opens
         assert no_opens == expected_no_opens
+
+        alpha, beta = self.rescaler.rescale_prior(rec, 10, 20)
+        assert alpha == 10 * PESSIMISTIC_PRIOR_ALPHA_SCALE_SUBTOPIC
+        assert beta == 20
+
+    def test_rescale_when_not_subtopic_item(self):
+        """Test when no experiment in request"""
+        rec = Mock()
+        rec.in_experiment.return_value = False
+        rec.isTimeSensitive = False
+
+        opens, no_opens = self.rescaler.rescale(rec, 100, 50)
+        assert opens == 100
+        assert no_opens == 50
 
         alpha, beta = self.rescaler.rescale_prior(rec, 10, 20)
         assert alpha == 10 * PESSIMISTIC_PRIOR_ALPHA_SCALE
         assert beta == 20
 
-    def test_rescale_when_not_in_experiment(self):
-        """Test when no experiment in request"""
-        rec = Mock()
-        rec.in_experiment.return_value = False
 
-        opens, no_opens = self.rescaler.rescale(rec, 100, 50)
-
-        assert opens == 100
-        assert no_opens == 50
-
-
-class TestCrawlerExperimentRescaler:
+class TestSchedulerHoldbackRescaler:
     """Test Rig for the rescaler"""
 
     def setup_method(self):
         """Set up test"""
-        self.rescaler = CrawlerExperimentRescaler(
-            subtopic_relative_size=SUBSECTION_EXPERIMENT_PERCENT
-        )
+        self.rescaler = SchedulerHoldbackRescaler()
 
     def test_rescale_subtopic_item(self):
-        """Test rescaling of priors for relative experiment size"""
+        """Not an expected use case for legacy sections"""
         rec = Mock()
         rec.in_experiment.return_value = True
-        rec.isTimeSensitive = False
         opens, no_opens = self.rescaler.rescale(rec, 100, 50)
-        expected_opens = 100 / SUBSECTION_EXPERIMENT_PERCENT
-        expected_no_opens = 50 / SUBSECTION_EXPERIMENT_PERCENT
+        expected_opens = 100 / SECTIONS_HOLDBACK_TOTAL_PERCENT
+        expected_no_opens = 50 / SECTIONS_HOLDBACK_TOTAL_PERCENT
         assert opens == expected_opens
         assert no_opens == expected_no_opens
 
         alpha, beta = self.rescaler.rescale_prior(rec, 40, 20)
-
-        assert alpha == 40 * PESSIMISTIC_PRIOR_ALPHA_SCALE
+        assert alpha == 40
         assert beta == 20
 
     def test_rescale_regular_item(self):
@@ -82,26 +81,9 @@ class TestCrawlerExperimentRescaler:
 
         opens, no_opens = self.rescaler.rescale(rec, 100, 50)
 
-        assert opens == 100 / CRAWLED_TOPIC_TOTAL_PERCENT
-        assert no_opens == 50 / CRAWLED_TOPIC_TOTAL_PERCENT
+        assert opens == 100 / SECTIONS_HOLDBACK_TOTAL_PERCENT
+        assert no_opens == 50 / SECTIONS_HOLDBACK_TOTAL_PERCENT
 
         alpha, beta = self.rescaler.rescale_prior(rec, 40, 20)
-
-        assert alpha == 40 * PESSIMISTIC_PRIOR_ALPHA_SCALE
-        assert beta == 20
-
-    def test_rescale_regular_boosted_item(self):
-        """Test when no experiment in request"""
-        rec = Mock()
-        rec.in_experiment.return_value = False
-        rec.isTimeSensitive = True
-        opens, no_opens = self.rescaler.rescale(rec, 100, 50)
-
-        assert opens == 100 / CRAWLED_TOPIC_TOTAL_PERCENT
-        assert no_opens == 50 / CRAWLED_TOPIC_TOTAL_PERCENT
-
-        alpha, beta = self.rescaler.rescale_prior(rec, 40, 20)
-
-        # alpha, beta are unchanged
         assert alpha == 40
         assert beta == 20
