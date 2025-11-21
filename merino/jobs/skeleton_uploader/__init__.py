f168488601e4bb044e615231875690027212f283
jrconlin <jr+git@mozilla.com>
2025-11-21 14:33:13 -0800

    f add job unit test skeleton

--- merino/jobs/skeleton_uploader/__init__.py
+++ merino/jobs/skeleton_uploader/__init__.py
@@ -24,7 +24,7 @@ from dynaconf.base import LazySettings
 
 import typer
 
-from merino.configs import settings as config
+from merino.configs import settings
 from merino.configs.app_configs.config_logging import configure_logging
 
 # Remote Settings accepts CSV files as the upload, so we need to convert
@@ -38,7 +38,7 @@ from merino.configs.app_configs.config_logging import configure_logging
 # ## Errors
 
 
-class GeneralError(BaseException):
+class SkeletonError(BaseException):
     """General purpose error. Specialize accordingly."""
 
     msg: str = "An error has occurred"
@@ -47,30 +47,31 @@ class GeneralError(BaseException):
         self.msg = msg
 
 
-class Options:
-    """Specify the options for the Skeleton app."""
+class SkeletonUploader:
+    """Perform the upload functions required for your Skeleton app.
+
+    Since this does not have to go through the Web interface, you do
+    not need to root objects to things like BaseModel.
+    """
 
     # Ensure that your local variables are declared.
     auth: str
-    other_option: str
+    logger: logging.Logger
 
-    def __init__(self, base_settings: LazySettings):
-        logger.debug("Defining options")
-        # """
-        self.auth = typer.Option(
-            "default",
-            "--auth",  # parameter declaration
-            help="Remote settings authorization token",
-        )
-        self.other_option = typer.Option("gorp", "--foo", help="Set something else")
-        # """
+    def __init__(self, auth: str | None):
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug("Defining options")
+        self.auth = auth or "NoAuth"
 
-    def get_command(self) -> typer.Typer:
-        """Define the app name and help screen"""
-        return typer.Typer(
-            name="skeleton_app",
-            help="A generic template app demonstrating merino ingestion",
-        )
+    def load_data(self) -> bool:
+        """Pretend to do things like fetch and store the data"""
+        self.logger.info(f"Uploading...{self.auth}")
+        # Get the data store reference
+        # Fetch the data from the provider
+        # Format it for storage
+        # Store the data
+        # Go have ice cream
+        return True
 
 
 # initialize our settings
@@ -78,7 +79,7 @@ class Options:
 # You can generally do `config.providers.{YourProject}`. I'm doing this
 # because this is a template.
 
-skeleton_settings = getattr(config.providers, "skeleton", None)
+skeleton_settings = getattr(settings.providers, "skeleton", {})
 """
 # Since we don't have any settings defined, we'll skip this check.
 if not skeleton_settings:
@@ -87,26 +88,27 @@ if not skeleton_settings:
     )
 #"""
 
-rs_settings = config.remote_settings
-options: Options = Options(base_settings=rs_settings)
-skeleton_cmd: typer.Typer = options.get_command()
+skeleton_cmd: typer.Typer = typer.Typer(
+    name="skeleton_app",
+    help="A generic template app demonstrating merino ingestion",
+)
 
 
 # Include this in the `cli.py` file to add the command to the general set.
 @skeleton_cmd.command()
 def upload(
-    auth: str = options.auth,
-    other: str = options.other_option,
+    auth: str | None = skeleton_settings.get("auth"),
 ):
     """Sample Upload function that just prints something. This text is used as the command help."""
-    logger = logging.getLogger(__name__)
-    logger.info(f"Uploading...{auth}")
     # This is a no-op since the skeleton has no meat, so no upload required.
     # Feel free to flesh this out with the things that you need to do.
+    # It's generally frowned upon to pass the LazySettings to the classes.
+    uploader = SkeletonUploader(auth=auth)
+    uploader.load_data()
 
 
 # This allows you to call this function outside of the `uv` construct.
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     # Logging is handled universally by `merino.configs.app_configs.configure_logging`
     # which uses `settings.logging.level` to specify the logging level
     # (DEBUG=10 .. CRITICAL=50)
