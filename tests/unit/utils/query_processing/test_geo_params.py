239ec5ff8518726f0830abde19099bf3bf1d052f
Tif Tran <ttran@mozilla.com>
2026-01-28 12:45:08 -0800

    [DISCO-3921] PII skeleton (#1249)
    * [DISCO-3921] PII skeleton
    
    * feedback changes

--- tests/unit/utils/query_processing/test_geo_params.py
+++ tests/unit/utils/query_processing/test_geo_params.py
@@ -13,7 +13,7 @@ from fastapi import Request
 
 from merino.middleware import ScopeKey
 from merino.middleware.geolocation import Location
-from merino.utils.api.query_params import (
+from merino.utils.query_processing.geo_params import (
     get_accepted_languages,
     refine_geolocation_for_suggestion,
     validate_suggest_custom_location_params,
