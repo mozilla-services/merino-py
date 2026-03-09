1e2de7e4730ae96196a8d2588846adc3589e8644
Temisan Iwere <tiwere@mozilla.com>
2026-03-09 21:20:34 +0100

    draft for fetching and storing engagement data

---
+++ merino/jobs/engagement_model/__init__.py
@@ -0,0 +1,75 @@
+import json
+import logging
+from typing import Any
+
+import typer
+
+from merino.jobs.engagement_model.amp_data_downloader import (
+    EngagementDataDownloader as AMPDownloader,
+)
+from merino.configs import settings
+from merino.jobs.engagement_model.wikipedia_data_downloader import (
+    EngagementDataDownloader as WikiDownloader,
+)
+
+from merino.utils.gcs.gcs_uploader import GcsUploader
+
+logger = logging.getLogger(__name__)
+
+
+cli = typer.Typer(
+    name="upload_engagement_data",
+    help="Commands to fetch and upload user engagemend data to GCS",
+)
+
+
+@cli.command()
+def upload_engagement_data():  # pragma: no cover
+    """Fetch AMP + Wikipedia engagement data and upload a JSON payload to GCS."""
+    logger.info("Starting engagement data pipeline...")
+
+    gcs_bq_project = settings.engagement.gcs_bq_project
+    gcs_storage_bucket = settings.engagement.gcs_storage_bucket
+    gcs_storage_project = settings.engagement.gcs_storage_project
+
+    amp_data_downloader = AMPDownloader(gcs_bq_project)
+    wiki_data_downloader = WikiDownloader(gcs_bq_project)
+
+    amp_data: list[dict[str, Any]] = amp_data_downloader.download_data()
+    wiki_data: dict[str, int] = wiki_data_downloader.download_data()
+
+    amp_aggregated = {
+        "impressions": sum(int(row["impressions"]) for row in amp_data),
+        "clicks": sum(int(row["clicks"]) for row in amp_data),
+    }
+
+    payload = {
+        "amp": amp_data,
+        "wiki_aggregated": {
+            "impressions": int(wiki_data["impressions"]),
+            "clicks": int(wiki_data["clicks"]),
+        },
+        "amp_aggregated": amp_aggregated,
+    }
+
+    content = json.dumps(payload, indent=2)
+
+    timestamp = ""  # TODO
+    destination_name = (
+        f"suggest-merino-exports/engagement/engagement_data_{timestamp}.json"
+    )
+
+    uploader = GcsUploader(
+        destination_gcp_project=gcs_storage_project,
+        destination_bucket_name=gcs_storage_bucket,
+        destination_cdn_hostname="",
+    )
+
+    uploader.upload_content(
+        content=content,
+        destination_name=destination_name,
+        content_type="application/json",
+        forced_upload=True,
+    )
+
+    logger.info("Uploaded engagement data")
---
+++ merino/jobs/engagement_model/wikipedia_data_downloader.py
@@ -0,0 +1,40 @@
+"""Download engagement metrics for Wikipedia from Biq Query table"""
+
+from typing import Any
+
+from google.cloud.bigquery import Client
+
+
+class EngagementDataDownloader:
+    """Download engagement data for Wikipedia"""
+
+    QUERY = """
+SELECT
+  COUNT(*) AS impressions,
+  COUNTIF(
+      product_selected_result = res.product_result_type
+      AND event_action = 'engaged'
+  ) AS clicks
+FROM `moz-fx-data-shared-prod.firefox_desktop.urlbar_events`
+CROSS JOIN UNNEST(results) AS res
+WHERE submission_date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) AND CURRENT_DATE()
+AND res.product_result_type = "wikipedia_dynamic"
+AND is_terminal
+AND normalized_channel = "release"
+"""
+    client: Client
+
+    def __init__(self, source_gcp_project: str) -> None:
+        self.client = Client(source_gcp_project)
+
+    def download_data(self) -> dict[str, Any]:
+        """Download engagement data from BigQuery and return formatted JSON objects"""
+
+        query_job = self.client.query(self.QUERY)
+        row = next(query_job.result(), None)
+        if row is None:
+            return {"impressions": 0, "clicks": 0}
+        return {
+            "impressions": row["impressions"],
+            "clicks": row["clicks"],
+        }
