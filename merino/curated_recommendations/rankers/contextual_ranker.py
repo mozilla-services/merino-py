dc83e3a6f73877dccc6e3b0c5470e0a423599251
Rolf Rando <rrando@mozilla.com>
2026-03-17 13:16:22 -0700

    fix

--- merino/curated_recommendations/rankers/contextual_ranker.py
+++ merino/curated_recommendations/rankers/contextual_ranker.py
@@ -34,7 +34,7 @@ from merino.curated_recommendations.rankers.utils import (
 # we're looking at are total impressions and we care about impressions with the inferred interests. When
 # contexual is rolled out we can use a dynamic computed value based on daily impressions.
 
-CONTEXUAL_AVG_BETA_VALUE = 10000
+CONTEXUAL_AVG_BETA_VALUE = 12000
 CONTEXTAL_LIMIT_PERCENTAGE_ADJUSTMENT = (
     0.5  # Underscored items tend to scale higher, leading to too much fresh content
 )
