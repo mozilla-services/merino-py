ad099502890d650f96f0b3527f98fa34c0825383
Herraj Luhano <hluhano@mozilla.com>
2026-04-08 13:41:47 -0400

    [HNT-2154] Add skeleton for wikimedia potd rss provider (#1368)
    * [HNT-2154] Add skeleton for wikimedia potd rss provider
    
    * add unit tests
    
    * pr feedback: add correct type for get provider method and update __init__()

---
+++ merino/providers/rss/__init__.py
@@ -0,0 +1,42 @@
+"""Initialize all RSS providers."""
+
+import logging
+
+from merino.providers.rss.base import BaseRssProvider
+from merino.providers.rss.wikimedia_potd.provider import WikimediaPotdProvider
+from merino.providers.rss.manager import load_providers
+
+logger = logging.getLogger(__name__)
+
+providers: dict[str, BaseRssProvider] = {}
+
+
+async def init_providers() -> None:
+    """Initialize all RSS providers.
+
+    This should only be called once at the startup of the application.
+    """
+    # load_providers() pulls all the rss providers from the config file.
+    providers.update(load_providers())
+    for name, provider in providers.items():
+        await provider.initialize()
+        logger.info("RSS provider initialized", extra={"provider": name})
+
+
+async def shutdown_providers() -> None:
+    """Shut down all RSS providers.
+
+    This should only be called once at the shutdown of the application.
+    """
+    for name, provider in providers.items():
+        await provider.shutdown()
+        # remove provider from
+        providers.pop(name)
+        logger.info("RSS provider shut down", extra={"provider": name})
+
+
+def get_wikimedia_potd_provider() -> WikimediaPotdProvider:
+    """Return the Wikimedia Picture of the Day provider."""
+    provider = providers["wikimedia_potd"]
+    assert isinstance(provider, WikimediaPotdProvider)
+    return provider
---
+++ merino/providers/rss/manager.py
@@ -0,0 +1,44 @@
+"""RSS provider manager."""
+
+from enum import Enum, unique
+
+from dynaconf.base import Settings
+
+from merino.configs import settings
+from merino.providers.rss.base import BaseRssProvider
+from merino.providers.rss.wikimedia_potd.provider import WikimediaPotdProvider
+from merino.utils.metrics import get_metrics_client
+
+
+@unique
+class RssProviderType(str, Enum):
+    """Enum for RSS provider type."""
+
+    WIKIMEDIA_POTD = "wikimedia_potd"
+
+
+def _create_provider(provider_id: str, setting: Settings) -> BaseRssProvider:
+    """Create an RSS provider for a given type and settings.
+
+    Exceptions:
+      - `ValueError` if the provider type is unknown.
+    """
+    match setting.type:
+        case RssProviderType.WIKIMEDIA_POTD:
+            return WikimediaPotdProvider(
+                backend=None,
+                metrics_client=get_metrics_client(),
+                name=provider_id,
+                query_timeout_sec=setting.query_timeout_sec,
+                enabled_by_default=setting.enabled_by_default,
+            )
+        case _:
+            raise ValueError(f"Unknown RSS provider type: {setting.type}")
+
+
+def load_providers() -> dict[str, BaseRssProvider]:
+    """Load RSS providers from configuration."""
+    providers: dict[str, BaseRssProvider] = {}
+    for provider_id, setting in settings.rss_providers.items():
+        providers[provider_id] = _create_provider(provider_id, setting)
+    return providers
