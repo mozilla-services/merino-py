59d4d67fdd5f2466f778d7727d74ef3422a72d17
Kat Schelonka <kschelonka@mozilla.com>
2026-04-15 15:37:48 -0700

    chore: move settings to module level

---
+++ merino/utils/logos.py
@@ -0,0 +1,78 @@
+"""Logos utility"""
+
+import logging
+from datetime import datetime
+from enum import StrEnum
+from functools import cache
+from typing import Optional
+import orjson
+from importlib.resources import files
+from urllib.parse import urljoin
+
+from pydantic import BaseModel, HttpUrl
+
+from merino.utils.metrics import get_metrics_client
+from merino.configs import settings
+
+logger = logging.getLogger(__name__)
+metrics_client = get_metrics_client()
+
+HOST_NAME: str = settings.image_gcs_v2.cdn_hostname
+BUCKET: str = settings.image_gcs_v2.gcs_bucket
+
+
+class LogoCategory(StrEnum):
+    """Enumeration of logo categories available in GCS."""
+
+    Airline = "airline"
+    MLB = "mlb"
+    NBA = "nba"
+    NFL = "nfl"
+    NHL = "nhl"
+
+
+class Logo(BaseModel):
+    """A single lookup entry from the lookup manifest."""
+
+    name: str
+    url: str
+
+
+class LogoManifest(BaseModel):
+    """Manifest mapping logo categories and keys to logo metadata."""
+
+    generated_at: datetime
+    lookups: dict[LogoCategory, dict[str, Logo]]
+
+    def get(self, category: LogoCategory, key: str) -> Logo | None:
+        """Return the LookupEntry for a given category and key, or None if not found."""
+        return self.lookups.get(category, {}).get(key.upper())
+
+
+@cache
+def load_manifest() -> LogoManifest:
+    """Load manifest and cache in memory"""
+    manifest_path = files("merino.data") / "logos_manifest.json"
+    return LogoManifest.model_validate(orjson.loads(manifest_path.read_bytes()))
+
+
+def get_logo_url(category: LogoCategory, key: str) -> Optional[HttpUrl]:
+    """Logo lookup for suggest providers.
+
+    The process for creating suggestions is currently manual,
+    and must follow the LogoManifest schema.
+
+    Each category dict is comprised of lookup keys to identify
+    a particular entity in the category.
+    For airlines, that is the 2-letter IATA code (lowercased).
+    For sports teams, it corresponds to the "key" field in the
+    `merino.providers.suggest.sports.backends.sportsdata.common.Team`
+    model.
+    """
+    logo = load_manifest().get(category, key)
+    if logo is None:
+        logger.warning(f"Logo does not exist for category={category} and key={key}")
+        metrics_client.increment("manifest.lookup", tags={"name": "logos", "result": "miss"})
+        return None
+    protocol = "http" if "localhost" in HOST_NAME else "https"
+    return HttpUrl(urljoin(f"{protocol}://{HOST_NAME}", f"{BUCKET}/{logo.url}"))
