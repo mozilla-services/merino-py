7d945a4182e1313dc70805f666e4d5f24bd32285
Nan Jiang <njiang028@gmail.com>
2026-05-13 11:53:52 -0400

    fix CI failures

--- tests/unit/test_runtime_modes.py
+++ tests/unit/test_runtime_modes.py
@@ -322,8 +322,8 @@ def test_lifespan_starts_and_stops_services_by_mode(
 
     events: list[str] = []
 
-    def record(event: str) -> Callable[[], None]:
-        def _record() -> None:
+    def record(event: str) -> Callable[..., None]:
+        def _record(*args, **kwargs) -> None:
             events.append(event)
 
         return _record
