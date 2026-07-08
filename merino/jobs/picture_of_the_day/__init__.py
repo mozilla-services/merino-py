3d1363ee4edfcc445b01d79cdcc22e300a9f1b79
Herraj Luhano <hluhano@mozilla.com>
2026-07-08 11:46:07 -0400

    [HNT-2775] Add cli command for wikimedia potd update job (#1651)
    * [HNT-2775] Add cli command for wikimedia potd update job
    
    * add cli command to merino jobs init
    
    * fix keyerror
    
    * use asyncio run to run the command and update success log
    
    * omit test coverage for private method for the job

---
+++ merino/jobs/picture_of_the_day/__init__.py
@@ -0,0 +1,30 @@
+"""CLI commands for the Wikimedia Picture of the Day updater job."""
+
+import asyncio
+import logging
+import typer
+from merino.providers.rss import get_wikimedia_potd_provider, init_providers
+
+logger = logging.getLogger(__name__)
+
+cli = typer.Typer(
+    name="wikimedia_potd_updater",
+    help="Commands to process and update data related to Picture of the Day widget on New Tab.",
+)
+
+
+async def _potd_update_job() -> bool:  # pragma: no cover
+    """Get the provider and call the method to upload potd."""
+    await init_providers()
+    provider = get_wikimedia_potd_provider()
+    return await provider.upload_picture_of_the_day()
+
+
+@cli.command("update-wikimedia-potd")
+def update_wikimedia_potd():  # pragma: no cover
+    """Execute the process to attempt to update the Wikimedia Picture of the Day."""
+    logger.info("Beginning wikimedia potd update process...")
+
+    success = asyncio.run(_potd_update_job())
+
+    logger.info("Wikimedia potd update job finished", extra={"success": success})
